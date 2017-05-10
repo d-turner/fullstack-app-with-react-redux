c311187ec0ac3d9b75221de9955f4cc3daaf7ab0
Daniel Turner <dturner@tcd.ie>
2017-05-10 15:35:54 +0100

    add sample data

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,12 +3,14 @@ import { combineReducers } from 'redux';
 import todoReducer from './todoReducer';
 import userReducer from './userReducer';
 import projectReducer from './projectReducer';
+import segmentReducer from './segmentReducer';
 
 // Reducer Root
 const reducerRoot = combineReducers({
   todoReducer,
   userReducer,
   projectReducer,
+  segmentReducer,
 });
 
 export default reducerRoot;
--- client/src/reducers/segmentReducer.js
+++ client/src/reducers/segmentReducer.js
@@ -1,5 +1,19 @@
 const intialState = {
-  segments: [
+  segments: [{
+    id: 0,
+    title: 'First',
+    description: 'Consequat voluptate consectetur fugiat veniam consequat nulla ipsum eu sunt.',
+  },
+  {
+    id: 1,
+    title: 'Second',
+    description: 'Tempor laboris cillum dolor cillum esse officia nulla non elit.',
+  },
+  {
+    id: 2,
+    title: 'Third',
+    description: 'Sit exercitation sint ea ullamco est reprehenderit proident reprehenderit ipsum et.',
+  },
   ],
 };
 
