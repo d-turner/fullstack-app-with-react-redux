1fee32c9ef17b74f9548b6b4dc895cb71b946c6d
Daniel Turner <dturner@tcd.ie>
2017-05-11 14:20:54 +0100

    rewrote segment list to use React.Component, this allows for live reloading, added more css

--- client/src/components/Segments/SegmentList.js
+++ client/src/components/Segments/SegmentList.js
@@ -2,29 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const SegmentList = (props) => {
-  const link = {
-    color: 'black',
-    textDecoration: 'none',
-    padding: '8',
-  };
+class SegmentList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const linkBlock = {
+      padding: '10px',
+      width: '520px',
+      backgroundColor: 'lightcoral',
+    };
+    const link = {
+      color: 'black',
+      textDecoration: 'none',
+      fontSize: '18px',
+      backgroundColor: 'blue',
+    };
+    const wrapper = {
+      backgroundColor: 'lightblue',
+      marginRight: '0px',
+      width: '520px',
+    };
 
-  const renderSegment = (segment, index) =>
+    const renderSegment = (segment, index) =>
     (
-      <div key={segment.id.toString()} value={index} className="data-list-item">
-        <Link to="/" style={link}>
-          <div><span>Segment: {segment.id + 1}</span></div>
-          <div><span>{segment.description}</span></div>
-        </Link>
+      <div key={segment.id.toString()} value={index} className="data-list-item" style={linkBlock}>
+        <div style={wrapper}>
+          <Link to={`/segments/${segment.id}`} style={link}>
+            <div><span>Segment: {segment.id + 1}</span></div>
+            <div><span>{segment.source}</span></div>
+          </Link>
+        </div>
       </div>
     );
 
-  return (
-    <div className="data-list">
-      {props.segments.map(renderSegment)}
-    </div>
-  );
-};
+    return (
+      <div className="data-list">
+        {this.props.segments.map(renderSegment)}
+      </div>
+    );
+  }
+}
 
 SegmentList.propTypes = {
   segments: PropTypes.arrayOf(PropTypes.object).isRequired,
