e099a1f6d632d76ff8735f849d7a2262fb0eb95f
Daniel Turner <dturner@tcd.ie>
2017-05-15 15:03:08 +0100

    add BlockStylecontrols and InlineStyleControls components, TODO: refactor these into their own component(s)

--- client/src/components/Segments/Segment.js
+++ client/src/components/Segments/Segment.js
@@ -134,4 +134,84 @@ const mapDispatchToProps = function(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 };
 
+
+class StyleButton extends React.Component {
+  constructor() {
+    super();
+    this.onToggle = (e) => {
+      e.preventDefault();
+      this.props.onToggle(this.props.style);
+    };
+  }
+  render() {
+    let active = this.props.className;
+    if (this.props.active) {
+      active += ` ${styles['RichEditor-activeButton']}`;
+    }
+    return (
+      <span className={active} onMouseDown={this.onToggle}>
+        {this.props.label}
+      </span>
+    );
+  }
+}
+const BLOCK_TYPES = [
+  { label: 'H1', style: 'header-one' },
+  { label: 'H2', style: 'header-two' },
+  { label: 'H3', style: 'header-three' },
+  { label: 'H4', style: 'header-four' },
+  { label: 'H5', style: 'header-five' },
+  { label: 'H6', style: 'header-six' },
+  { label: 'Blockquote', style: 'blockquote' },
+  { label: 'UL', style: 'unordered-list-item' },
+  { label: 'OL', style: 'ordered-list-item' },
+  { label: 'Code Block', style: 'code-block' },
+];
+const BlockStyleControls = (props) => {
+  const { editorState } = props;
+  const selection = editorState.getSelection();
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey())
+    .getType();
+  return (
+    <div>
+      {BLOCK_TYPES.map(type =>
+        <StyleButton
+          className={styles['RichEditor-styleButton']}
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />,
+      )}
+    </div>
+  );
+};
+
+const INLINE_STYLES = [
+  { label: 'Bold', style: 'BOLD' },
+  { label: 'Italic', style: 'ITALIC' },
+  { label: 'Underline', style: 'UNDERLINE' },
+  { label: 'Monospace', style: 'CODE' },
+];
+const InlineStyleControls = (props) => {
+  const currentStyle = props.editorState.getCurrentInlineStyle();
+  return (
+    <div>
+      {INLINE_STYLES.map(type =>
+        <StyleButton
+          className={styles['RichEditor-styleButton']}
+          key={type.label}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />,
+      )}
+    </div>
+  );
+};
+
 export default connect(mapStateToProps, mapDispatchToProps)(Segment);
