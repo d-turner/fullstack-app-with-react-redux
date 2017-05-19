f36bfdeb5d697e3c76eaf0d679bf946dbd56b005
Daniel Turner <dturner@tcd.ie>
2017-05-19 17:24:53 +0100

    add Document presentation layer (need to update it to use the segmentlist)

---
+++ client/src/components/Document/Presentation/Document.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Document(props) {
+  return (
+    <div className="data-list">
+      <span>HERE!</span>
+    </div>
+  );
+}
+
+Document.propTypes = {
+  //document: PropTypes.objectOf(PropTypes.object).isRequired,
+};
