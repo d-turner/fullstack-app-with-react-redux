7ad5964e0f22e4fb87c87f783fe85cda793e7b50
Daniel Turner <dturner@tcd.ie>
2017-05-19 17:04:31 +0100

    add loggerMiddleware (can be removed), add thinkMiddleware

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,18 @@
-import {createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
 import reducerRoot from './reducers';
 
-const store = createStore(reducerRoot, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  reducerRoot,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(
+    thunkMiddleware, // lets us dispatch() functions
+    loggerMiddleware, // neat middleware that logs actions
+  ),
+);
 
 export default store;
