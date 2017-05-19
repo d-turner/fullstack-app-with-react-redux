86352da580dd26c86cdffe32b3efe7737b006d9f
Daniel Turner <dturner@tcd.ie>
2017-05-19 15:21:28 +0100

    refactored and updated structure

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,7 +1,15 @@
 /*
  * Action Constants
  */
+// Document Actions
+export const FETCH_DOCUMENT = 'FETCH_DOCUMENT_REQUEST';
+export const FETCH_DOCUMENT_FAIL = 'FETCH_DOCUMENT_FAIL';
+export const FETCH_DOCUMENT_SUC = 'FETCH_DOCUMENT_SUCCESS';
+
+// Segment Actions
 export const UPDATE_TARGET = 'UPDATE_TARGET';
+
+// Project Actions
 export const ADD_PROJECT = 'ADD_PROJECT';
 export const REMOVE_PROJECT = 'REMOVE_PROJECT';
 export const POP_PROJECTS = 'POPULATE_PROJECTS';
