b87bb6e1ea62e2430edc873a86d0f011cb1d1d22
Daniel Turner <dturner@tcd.ie>
2017-05-22 12:40:46 +0100

    add proper container control to the component

--- client/src/components/Document/Containers/DocumentContainer.js
+++ client/src/components/Document/Containers/DocumentContainer.js
@@ -3,33 +3,46 @@ import PropTypes from 'prop-types';
 
 import Document from '../Presentation/Document';
 
+
+const documentClass = {
+  margin: '10px',
+  padding: '10px',
+  width: '520px',
+  backgroundColor: 'lightcoral',
+};
+
 class DocumentContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { document: {} };
   }
 
   componentDidMount() {
-    this.props.requestDocument('machines.xlf').then(() => {
-      console.log('here');
-      console.log(this);
-    });
-
-    this.props.requestDocument('en-jpExample.xlf').then(() => {
-      console.log('here');
-      console.log(this);
-    });
+    this.props.requestDocument('machines.xlf');
+    this.props.requestDocument('en-jpExample.xlf');
   }
 
   render() {
+    const renderDocument = (key, index) =>
+    (
+      <div key={key} value={key}>
+        <div style={documentClass}>
+          Document: {index + 1}
+          <Document documentName={key} document={this.props.documents[key]} />
+        </div>
+      </div>
+    );
+
     return (
-      <Document {...this.props} />
+      <div>
+        {Object.keys(this.props.documents).map(renderDocument)}
+      </div>
     );
   }
 }
 
 DocumentContainer.propTypes = {
   requestDocument: PropTypes.func.isRequired,
+  documents: PropTypes.objectOf(PropTypes.object).isRequired,
 };
 
 export default DocumentContainer;
