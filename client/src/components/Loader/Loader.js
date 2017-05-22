80b64f293af7fa5c1feced2a96790fb663badf97
Daniel Turner <dturner@tcd.ie>
2017-05-22 12:42:52 +0100

    add css only loader component

---
+++ client/src/components/Loader/Loader.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from './Loader.css';
+
+export default function Loader() {
+  return (
+    <div className="data-list">
+      <span>Loading...</span>
+      <div className={styles['sk-circle']}>
+        <div className={`${styles['sk-circle1']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle2']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle3']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle4']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle5']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle6']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle7']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle8']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle9']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle10']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle11']} ${styles['sk-child']}`} />
+        <div className={`${styles['sk-circle12']} ${styles['sk-child']}`} />
+      </div>
+    </div>
+  );
+}
