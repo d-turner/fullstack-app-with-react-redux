167d190aede14771e6ab82469e698d9a0194e129
Daniel Turner <dturner@tcd.ie>
2017-05-22 12:37:57 +0100

    add Document information to be displayed by component Need to add a click handler for each document to that will bring the user to the segment list

--- client/src/components/Document/Presentation/Document.js
+++ client/src/components/Document/Presentation/Document.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Loader from '../../Loader/Loader';
+
 export default function Document(props) {
+  if (props.document && props.document.isFetching) {
+    return (
+      <div className="data-list">
+        <span>Status: <Loader /></span>
+      </div>
+    );
+  }
   return (
     <div className="data-list">
-      <span>HERE!</span>
+      <span>Status: Finished Loading <br /></span>
+      <span>Name: {props.documentName}</span>
+      <div>
+        <span>Source Language: {props.document.xliff.sourceLang} <br /></span>
+        <span>Target Language: {props.document.xliff.targetLang}</span>
+      </div>
     </div>
   );
 }
 
 Document.propTypes = {
-  //document: PropTypes.objectOf(PropTypes.object).isRequired,
+  document: PropTypes.objectOf(PropTypes.any).isRequired,
+  documentName: PropTypes.string.isRequired,
 };
