4118cb1f440ba0bc6d65ffde1d4a10854a0c097f
Daniel Turner <dturner@tcd.ie>
2017-05-24 16:55:30 +0100

    updated action creator to update a single segment of a document list

--- client/src/components/Segment/ActionCreators/SegmentActions.js
+++ client/src/components/Segment/ActionCreators/SegmentActions.js
@@ -1,12 +1,22 @@
 import * as actions from '../../../constants/actionTypes';
 
 // update translation
-export function updateSegment(id, target) {
+export function updateSegment(documentId, segmentId, editorState, target) {
   return {
     type: actions.UPDATE_TARGET,
     segment: {
-      id,
+      documentId,
+      segmentId,
+      editorState,
       target,
     },
+  };
+}
+
+// initialise segment reducer
+export function populateSegments(segmentArray) {
+  return {
+    type: actions.POP_SEGMENTS,
+    segments: segmentArray,
   };
 }
