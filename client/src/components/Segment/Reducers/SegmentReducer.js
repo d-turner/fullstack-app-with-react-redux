192ade5d7f900ac0af70ff50fb96da3c11d084cb
Daniel Turner <dturner@tcd.ie>
2017-05-24 16:54:42 +0100

    removed placeholder data and added a POPULATE action

--- client/src/components/Segment/Reducers/SegmentReducer.js
+++ client/src/components/Segment/Reducers/SegmentReducer.js
@@ -1,42 +1,7 @@
-import { EditorState, ContentState, convertFromRaw } from 'draft-js';
+import { EditorState, convertFromRaw } from 'draft-js';
 import * as actions from '../../../constants/actionTypes';
 
-const initialState = {
-  segments: [{
-    id: 0,
-    title: 'First',
-    description: 'Consequat voluptate consectetur fugiat veniam consequat nulla ipsum eu sunt.',
-    source: '<i>Reprehenderit qui minim</i> consequat minim occaecat.',
-    mt: '<b>Nostrud occaecat</b> aliqua eiusmod qui laboris eu dolore veniam sit reprehenderit.',
-    target: 'Nostrud occaecat aliqua eiusmod qui laboris eu dolore veniam sit reprehenderit.',
-    editorState: EditorState.createWithContent(
-      ContentState.createFromText('Nostrud occaecat aliqua eiusmod qui laboris eu dolore veniam sit reprehenderit.'),
-    ),
-  },
-  {
-    id: 1,
-    title: 'Second',
-    description: 'Tempor laboris cillum dolor cillum esse officia nulla non elit.',
-    source: 'Officia laborum esse nisi laboris.',
-    mt: 'Sunt laboris mollit nisi sit.',
-    target: 'Sunt laboris mollit nisi sit.',
-    editorState: EditorState.createWithContent(
-      ContentState.createFromText('Sunt laboris mollit nisi sit.'),
-    ),
-  },
-  {
-    id: 2,
-    title: 'Third',
-    description: 'Sit exercitation sint ea ullamco est reprehenderit proident reprehenderit ipsum et.',
-    source: 'Nisi deserunt consectetur cillum sunt deserunt dolor duis dolor labore sint excepteur do elit nostrud.',
-    mt: 'Eiusmod est aliqua id fugiat laboris deserunt in cillum occaecat duis.',
-    target: 'Eiusmod est aliqua id fugiat laboris deserunt in cillum occaecat duis.',
-    editorState: EditorState.createWithContent(
-      ContentState.createFromText('Eiusmod est aliqua id fugiat laboris deserunt in cillum occaecat duis.'),
-    ),
-  },
-  ],
-};
+const initialState = {};
 
 const updateSegmentTarget = (segment, action) => {
   if (segment.id !== action.segment.id) {
@@ -50,7 +15,11 @@ const updateSegmentTarget = (segment, action) => {
 
 const segmentReducer = function(state = initialState, action) {
   switch (action.type) {
-
+    case actions.POP_SEGMENTS:
+      return {
+        ...state,
+        segments: action.segments,
+      };
     case actions.UPDATE_TARGET:
       return {
         ...state,
