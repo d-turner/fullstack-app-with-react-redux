a54ca2fbdf0bf1b2eb971b7a0503d60c75c3a06e
Daniel Turner <dturner@tcd.ie>
2017-06-07 16:59:15 +0100

    add function to handle splitting a segment at the given cursor position
    add function to handle UPDATE_SELECTED action

--- client/src/components/Document/Reducers/DocumentReducer.js
+++ client/src/components/Document/Reducers/DocumentReducer.js
@@ -63,6 +63,34 @@ const updateTarget = function(state = blankDocument, action) {
   return state;
 };
 
+const splitSegment = function(state, action) {
+  if (state.id === action.documentId) {
+    const oldSegment = state.xliff.segments[action.segmentId];
+    const newSegment1 = Object.assign({}, oldSegment, {
+      source: oldSegment.source.substring(0, action.cursorPosition),
+      target: '',
+      editorState: EditorState.createEmpty(),
+    });
+    const newSegment2 = Object.assign({}, oldSegment, {
+      source: oldSegment.source.substring(action.cursorPosition),
+      target: '',
+      editorState: EditorState.createEmpty(),
+    });
+    const newSegments = [newSegment1, newSegment2];
+    const before = state.xliff.segments.slice(0, action.segmentId);
+    const after = state.xliff.segments.slice(action.segmentId + 1, state.xliff.segments.length);
+    const newArr = before.concat(newSegments, after);
+    return {
+      ...state,
+      xliff: {
+        ...state.xliff,
+        segments: newArr,
+      },
+    };
+  }
+  return state;
+};
+
 const DocumentListReducer = function(state = initialState, action) {
   switch (action.type) {
     case actions.FETCH_DOCUMENT:
@@ -79,12 +107,28 @@ const DocumentListReducer = function(state = initialState, action) {
       });
     case actions.UPDATE_TARGET:
       return Object.assign({}, state, {
+        editorState: action.segment.editorState,
         documents: state.documents.map(doc => updateTarget(doc, action)),
       });
     case actions.LOOKUP:
       return {
         ...state,
         lexicon: action.lexicon,
+      };
+    case actions.SPLIT:
+      return Object.assign({}, state, {
+        documents: state.documents.map(doc => splitSegment(doc, action)),
+      });
+    case actions.UPDATE_SELECTED:
+      if (state.selectedSegment === action.segmentId) {
+        return state;
+      }
+      return {
+        ...state,
+        selectedSegment: action.segmentId,
+        editorState: EditorState.createWithContent(ContentState.createFromText(
+          state.documents[action.documentId].xliff.segments[action.segmentId].target,
+        )),
       };
     default:
       return state;
