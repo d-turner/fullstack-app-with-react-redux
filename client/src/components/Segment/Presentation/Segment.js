182786f5c25ca1de51b8b165fad30a63409520f1
Daniel Turner <dturner@tcd.ie>
2017-06-07 16:48:36 +0100

    add logic to only rnder the editor if the current segment is the selected segment, removed labels for source, target, etc

--- client/src/components/Segment/Presentation/Segment.js
+++ client/src/components/Segment/Presentation/Segment.js
@@ -5,31 +5,40 @@ import styles from '../styles.css';
 import CustomEditor from '../../Editor/CustomEditor';
 
 class Segment extends React.Component {
+  renderEditor() {
+    if (this.props.selectedSegment === this.props.segmentId) {
+      return (
+        <div className={styles.editorWrapper} onClick={this.props.focus} tabIndex={0} role={'textbox'} >
+          <CustomEditor
+            editorState={this.props.editorState}
+            toggleBlockType={this.props.toggleBlockType}
+            toggleInlineStyle={this.props.toggleInlineStyle}
+            handleKeyCommand={this.props.handleKeyCommand}
+            handleChange={this.props.handleChange}
+            ref={(ref) => { this.CustomEditor = ref; }}
+          />
+        </div>
+      );
+    }
+    return (
+      <div />
+    );
+  }
+
   render() {
     return (
-      <div className={styles.format}>
-        <div style={{ marginTop: '20px' }}>
-          <span>Source:</span>
-          <div className={styles.wrapper}>{this.props.segment.source}</div>
-        </div>
+      <div className={`${styles.format}`}>
+        {/* <span>Source:</span> */}
+        <div className={styles.wrapper}>{this.props.segment.source}</div>
 
         <div style={{ marginTop: '20px' }}>
-          <span>Machine Translation:</span>
+          {/* <span>Machine Translation:</span> */}
           <div className={styles.wrapper} dangerouslySetInnerHTML={{ __html: this.props.segment.target }} />
         </div>
 
         <div style={{ marginTop: '20px' }}>
-          <span>Target:</span>
-          <div className={styles.wrapper} onClick={this.props.focus} tabIndex={0} role={'textbox'} >
-            <CustomEditor
-              editorState={this.props.editorState}
-              toggleBlockType={this.props.toggleBlockType}
-              toggleInlineStyle={this.props.toggleInlineStyle}
-              handleKeyCommand={this.props.handleKeyCommand}
-              handleChange={this.props.handleChange}
-              ref={(ref) => { this.CustomEditor = ref; }}
-            />
-          </div>
+          {/* <span>Target:</span> */}
+          {this.renderEditor()}
         </div>
       </div>
     );
@@ -45,6 +54,7 @@ Segment.propTypes = {
   segment: PropTypes.objectOf(PropTypes.any).isRequired,
   editorState: PropTypes.objectOf(PropTypes.any).isRequired,
   segmentId: PropTypes.number.isRequired,
+  selectedSegment: PropTypes.number.isRequired,
 };
 
 export default Segment;
