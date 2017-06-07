b0112962e02a38f1d23cfe6f08e77a481c2e139f
Daniel Turner <dturner@tcd.ie>
2017-06-07 16:32:00 +0100

    add action to SPLIT a segment, add action to UPDATE_SELECTED segment from the list of segments

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -10,6 +10,10 @@ export const FETCH_DOCUMENT_SUC = 'FETCH_DOCUMENT_SUCCESS';
 export const UPDATE_TARGET = 'UPDATE_TARGET';
 export const POP_SEGMENTS = 'POPULATE_SEGMENTS';
 export const LOOKUP = 'LOOKUP_LEXICON';
+export const SPLIT = 'SPLIT';
+
+// SegmentList Actions
+export const UPDATE_SELECTED = 'UPDATE_SELECTED_SEGMENT';
 
 // Project Actions
 export const ADD_PROJECT = 'ADD_PROJECT';
