d4ff4eb2230663fc72d8bda946f5f33cafeecf02
Daniel Turner <dturner@tcd.ie>
2017-06-07 16:35:01 +0100

    add 'bindActionCreators' to SegmentList, add global prop editorState, add global prop selectedSegment

--- client/src/components/SegmentList/Containers/index.js
+++ client/src/components/SegmentList/Containers/index.js
@@ -1,15 +1,23 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import SegmentContainer from './SegmentContainer';
+import * as actionCreators from '../ActionCreators/SegmentListActions';
 
 const mapStateToProps = function(state) {
   // get the required reducer(s) from the state
   const { documentReducer } = state;
   // return what we want available in the props
-  const { documents } = documentReducer;
+  const { documents, editorState, selectedSegment } = documentReducer;
   return {
-    documents,
+    documents, editorState, selectedSegment,
   };
 };
 
-export default connect(mapStateToProps)(SegmentContainer);
+const mapDispatchToProps = function(dispatch) {
+  // get the available dispatch actions
+  return bindActionCreators(actionCreators, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SegmentContainer);
+
