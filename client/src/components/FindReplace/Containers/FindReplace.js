d9d9984903887606f55df1485873bd0be6ff1dfb
Daniel Turner <dturner@tcd.ie>
2017-06-13 17:08:07 +0100

    Remove Prev - Too much logic Add Replace function Clean up component

--- client/src/components/FindReplace/Containers/FindReplace.js
+++ client/src/components/FindReplace/Containers/FindReplace.js
@@ -6,39 +6,34 @@ class FindReplace extends React.Component {
     super(props);
 
     this.findNext = this.findNext.bind(this);
-    this.findPrev = this.findPrev.bind(this);
-    this.replaceText = this.replaceAll.bind(this);
+    this.replaceText = this.replaceText.bind(this);
     this.replaceAll = this.replaceAll.bind(this);
   }
 
   findNext() {
-    if (this.props.FindReplaceReducer.word !== '') {
+    if (this.props.findReplace.word !== '') {
       this.props.findNext(
         this.props.documentId,
-        this.props.FindReplaceReducer.currentSegment,
-        this.props.FindReplaceReducer.cursorPosition,
-        this.props.FindReplaceReducer.word
+        this.props.findReplace.currentSegment,
+        this.props.findReplace.wordIndex,
+        this.props.findReplace.word
       );
       setTimeout(() => {
         const location = document.getElementById('findreplace');
         location.scrollIntoView();
-        this.props.updateCurrentLocation(location.attributes['data-location'].value);
       }, 150);
     }
   }
 
-  findPrev() {
-    console.log('Find prev');
-    const text = 'text';
-    const newText = 'newText';
-    this.props.findNext(this.props.documentId, this.props.segmentId, this.props.segmentOffset, text);
-  }
-
   replaceText() {
-    console.log('replace text');
-    const text = 'text';
-    const newText = 'newText';
-    this.props.findNext(this.props.documentId, this.props.segmentId, this.props.segmentOffset, text, newText);
+    const newText = this.replaceText.value;
+    this.props.replaceText(
+      this.props.documentId,
+      this.props.findReplace.currentSegment,
+      this.props.findReplace.wordIndex,
+      this.props.findReplace.word,
+      newText,
+    );
   }
 
   replaceAll() {
@@ -49,12 +44,12 @@ class FindReplace extends React.Component {
   }
 
   render() {
-    if (this.props.FindReplaceReducer.render) {
+    if (this.props.findReplace.render) {
       return (
         <div>
           Find and Replace <br />
+          <div><input type="text" ref={(ref) => { this.replaceText = ref; }} /></div>
           <button onClick={this.findNext}>Next</button>
-          <button onClick={this.findPrev}>Previous</button>
           <button onClick={this.replaceText}>Replace</button>
           <button onClick={this.replaceAll}>Replace All</button>
         </div>
