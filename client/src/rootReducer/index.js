9edf5ccb85786b4e36993a5d57333cba1812ccc4
Daniel Turner <dturner@tcd.ie>
2017-06-13 16:13:29 +0100

    integrated the FindReplaceReducer into the document reducer

--- client/src/rootReducer/index.js
+++ client/src/rootReducer/index.js
@@ -3,13 +3,11 @@ import { combineReducers } from 'redux';
 import projectReducer from '../components/Project/Reducers/ProjectReducer';
 // import segmentReducer from '../components/Segment/Reducers/SegmentReducer';
 import documentReducer from '../components/Document/Reducers/DocumentReducer';
-import FindReplaceReducer from '../components/FindReplace/Reducers/FindReplaceReducer';
 
 // Reducer Root
 const reducerRoot = combineReducers({
   projectReducer,
   documentReducer,
-  FindReplaceReducer,
 });
 
 export default reducerRoot;
