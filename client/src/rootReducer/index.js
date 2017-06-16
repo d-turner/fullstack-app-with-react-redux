5b4e9965198f4301a246376c785e29899aeab0b5
Daniel Turner <dturner@tcd.ie>
2017-06-16 10:06:38 +0100

    add comment reducer to rootreducer

--- client/src/rootReducer/index.js
+++ client/src/rootReducer/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import projectReducer from '../components/Project/Reducers/ProjectReducer';
 // import segmentReducer from '../components/Segment/Reducers/SegmentReducer';
 import documentReducer from '../components/Document/Reducers/DocumentReducer';
+import commentReducer from '../components/Comments/Reducers/CommentReducer';
 
 // Reducer Root
 const reducerRoot = combineReducers({
   projectReducer,
   documentReducer,
+  commentReducer,
 });
 
 export default reducerRoot;
