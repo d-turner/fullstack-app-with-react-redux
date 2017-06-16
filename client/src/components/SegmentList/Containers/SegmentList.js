396e32624073f6fda9f69eaec11c7483be980405
Daniel Turner <dturner@tcd.ie>
2017-06-16 10:17:04 +0100

    add segment sidebar buttons for search, lexicon and comments

--- client/src/components/SegmentList/Containers/SegmentList.js
+++ client/src/components/SegmentList/Containers/SegmentList.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactHint from 'react-hint';
+import 'react-hint/css/index.css';
 
 import styles from '../styles.css';
-import Segment from '../../../components/Segment/Containers/Segment';
+import Segment from '../..//Segment/Containers/Segment';
+import { addComment } from '../../Comments/ActionCreators/CommentActions';
+import store from '../../../store.js';
 
 class SegmentList extends React.Component {
-  componentWillMount() {
-    console.warn('Need to fix this so it wont null the first segment');
+  constructor(props) {
+    super(props);
     this.props.updateSelectedSegment(this.props.documentId, 0);
   }
 
   selected(index) {
     this.props.updateSelectedSegment(this.props.documentId, index);
+  }
+
+  addComment(index) {
+    const comment = 'this is an action test';
+    const user = 'Daniel Turner';
+    const time = Date.now();
+    store.dispatch(addComment(this.props.documentId, index, comment, user, time));
   }
 
   render() {
@@ -28,16 +39,41 @@ class SegmentList extends React.Component {
       }
 
       return (
-        <button
-          onClick={() => this.selected(index)}
-          key={index} value={index} className={styles.block}
-          aria-label="Active the selected segment"
-          role={'textbox'}>
-          <div className={styles.segmentWrapper}> {/* another wrapper for row flex*/}
-            <div className={styles.segmentId}>{index}</div>{/* segment number*/}
-            <Segment documentId={this.props.documentId} segmentId={index} editorState={this.props.editorState} />
+        <div className={styles.segmentFlex}>
+          <button
+            onClick={() => this.selected(index)}
+            key={index} value={index} className={styles.block}
+            aria-label="Activate the selected segment"
+            role={'textbox'}>
+            <div className={styles.segmentWrapper}> {/* another wrapper for row flex*/}
+              <div className={styles.segmentId}>{index}</div>{/* segment number*/}
+              <Segment documentId={this.props.documentId} segmentId={index} editorState={this.props.editorState} />
+            </div>
+          </button>
+          <div className={`${styles['sidebar-handle']}`}>
+            <div className={`${styles['sidebar-fixed']}`}>
+              <button className={`${styles['sidebar-wrapper']} ${styles['sidebar-button']}`}
+                data-rh="Comment"
+                data-rh-at="right"
+                aria-label="Add a Comment"
+                onClick={() => this.addComment(index)}>
+                <i className="tiny material-icons">chat_bubble</i>
+              </button>
+              <button className={`${styles['sidebar-wrapper']} ${styles['sidebar-button']}`}
+                data-rh="Lexicon"
+                data-rh-at="right"
+                aria-label="">
+                <i className="small material-icons">translate</i>
+              </button>
+              <button className={`${styles['sidebar-wrapper']} ${styles['sidebar-button']}`}
+                data-rh="Search"
+                data-rh-at="right">
+                <i className="small material-icons">search</i>
+              </button>
+              <ReactHint />
+            </div>
           </div>
-        </button>
+        </div>
       );
     };
 
