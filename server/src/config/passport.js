381a3a2d37f69823678f0877790544490618245d
Daniel Turner <dturner@tcd.ie>
2017-06-20 14:34:05 +0100

    add passport configuration, may refactor this into a better location

---
+++ server/src/config/passport.js
@@ -0,0 +1,43 @@
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+
+import User from '../db/user';
+import logger from '../util/logger';
+
+// Serialize sessions
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  const id = user.user_id;
+  User.findOneUser(id, (err, result) => {
+    console.log(result);
+    done(err, result[0]);
+  });
+});
+
+// Use local strategy
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+},
+(username, password, cb) => {
+  User.findByUsername(username, (err, result) => {
+    const user = result[0];
+    if (err) { return cb(err); }
+    if (!user) { return cb(null, false); }
+    if (user.password !== password) { return cb(null, false); }
+    logger.info('User authenticated...');
+    return cb(null, user);
+  });
+},
+));
+
+passport.ensureAuthenticated = function ensureAuthenticated(req, res, next) {
+  if (req.user) { return next(req, res); }
+  res.status(401).send('You are not authenticated...');
+};
+
+
+export default passport;
