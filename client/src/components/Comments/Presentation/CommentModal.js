f8be2e5375212a8f70519e0815f80bfe9cb7724f
Daniel Turner <dturner@tcd.ie>
2017-06-22 10:07:27 +0100

    let the parent component deal with the rendering and unrenedering of the comment modal

--- client/src/components/Comments/Presentation/CommentModal.js
+++ client/src/components/Comments/Presentation/CommentModal.js
@@ -8,20 +8,11 @@ import store from '../../../store';
 export default class CommentModal extends React.Component {
   addComment(event, index) {
     event.preventDefault();
-    console.log(event.target);
     const comment = event.target[0].value;
     const user = 'Daniel Turner';
     const time = Date.now();
     store.dispatch(addComment(this.props.documentId, index, comment, user, time));
-    this.props.unrender(index, this.unrenderComment);
-  }
-
-  unrenderComment(index, array) {
-    const newArray = array.map((value, i) => {
-      if (index === i) return false;
-      return false;
-    });
-    return newArray;
+    this.props.unrender(index);
   }
 
   render() {
@@ -29,10 +20,14 @@ export default class CommentModal extends React.Component {
       <div>
         <div>Add Comment</div>
         <form onSubmit={event => this.addComment(event, this.props.index)}>
-          <input className={styles.commentInput} autoComplete="false" aria-label={`Comment input for segment ${this.props.index}`} />
+          <input className={styles.commentInput} autoComplete="false"
+            aria-label={`Comment input for segment ${this.props.index}`}
+          />
           <div className={styles.commentButtons} >
-            <button className={styles.commentButton} onClick={() => this.unrenderComment(this.props.index)}>Cancel</button>
             <button className={styles.commentButton} type="submit">Add</button>
+            <button className={styles.commentButton}
+              onClick={() => this.props.unrender(this.props.index)}>Cancel
+            </button>
           </div>
         </form>
       </div>
