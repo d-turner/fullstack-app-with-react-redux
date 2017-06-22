acaece94fd7d8972da77f803b73c505735c3c53a
Daniel Turner <dturner@tcd.ie>
2017-06-22 09:51:17 +0100

    update Comment sidebar

--- client/src/components/Comments/Containers/CommentSidebar.js
+++ client/src/components/Comments/Containers/CommentSidebar.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from '../../Sidebar/styles.css';
-import Comments from './CommentList';
+import styles from '../styles.css';
+import CommentList from './CommentList';
 
 class CommentSidebar extends React.Component {
   render() {
     return (
-      <div className={styles.parent}>
-        <div className={`${styles.flexItem} ${styles.lexicon}`}>
-          <h3>Comments</h3>
-          <div className={`${styles.wrapper} ${styles.overflow}`}>
-            <div className={styles.flexItem}>
-              <Comments documentId={this.props.documentId} {...this.props} />
-            </div>
+      <div>
+        <h3>Comments</h3>
+        <div className={`${styles.wrapper} ${styles.overflow}`}>
+          <div className={styles.flexItem}>
+            <CommentList documentId={this.props.documentId} {...this.props} />
           </div>
         </div>
       </div>
