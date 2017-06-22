19a8d4eac54605783265071c14e4bf7fb76e96bd
Daniel Turner <dturner@tcd.ie>
2017-06-22 10:19:38 +0100

    add connect logic

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -4,11 +4,13 @@ import Sidebar from './Containers/Sidebar';
 
 const mapStateToProps = function(state) {
   // get the required reducer(s) from the state
-  const { documentReducer } = state;
+  const { documentReducer, commentReducer } = state;
   // return what we want available in the props
   const { lexicon } = documentReducer;
+  const { renderComments } = commentReducer;
   return {
     lexicon,
+    renderComments,
   };
 };
 
