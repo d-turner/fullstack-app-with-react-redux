adb6a02e6be97e189ee01ed27c6a90fc8e7666a0
Daniel Turner <dturner@tcd.ie>
2017-06-23 14:54:08 +0100

    update to a class with functions for rendering the correct sidebar

--- client/src/components/SegmentList/Containers/SegmentContainer.js
+++ client/src/components/SegmentList/Containers/SegmentContainer.js
@@ -5,19 +5,65 @@ import SegmentList from './SegmentList';
 import Sidebar from '../../Sidebar/Containers/Sidebar';
 import styles from '../styles.css';
 
-function SegmentContainer(props) {
-  const id = parseInt(props.match.params.documentId, 10);
-  // const i = props.documents.findIndex(doc => doc.id === id);
-  const doc = props.documents[id];
+class SegmentContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    const id = parseInt(props.match.params.documentId, 10);
+    // const i = props.documents.findIndex(doc => doc.id === id);
+    this.renderComment = this.renderComment.bind(this);
+    this.renderLexicon = this.renderLexicon.bind(this);
+    this.renderComment = this.renderSearch.bind(this);
+    this.state = {
+      id,
+      renderComment: false,
+      renderLexicon: false,
+      renderSearch: false,
+    };
+  }
 
-  return (
-    <div className={styles.mainContent}>
-      <SegmentList segments={doc.xliff.segments} documentId={id} editorState={props.editorState} {...props} />
-      <Sidebar
-        documentId={id}
-      />
-    </div>
-  );
+  renderComment() {
+    this.setState({
+      renderComment: true,
+      renderLexicon: false,
+      renderSearch: false,
+    });
+  }
+
+  renderLexicon() {
+    this.setState({
+      renderComment: false,
+      renderLexicon: true,
+      renderSearch: false,
+    });
+  }
+
+  renderSearch() {
+    this.setState({
+      renderComment: false,
+      renderLexicon: false,
+      renderSearch: true,
+    });
+  }
+  render() {
+    return (
+      <div className={styles.mainContent}>
+        <SegmentList
+          segments={this.props.documents[this.state.id].xliff.segments}
+          documentId={this.state.id}
+          editorState={this.props.editorState}
+          renderComment={this.renderComment}
+          renderLexicon={this.renderLexicon}
+          renderSearch={this.renderSearch}
+          {...this.props} />
+        <Sidebar
+          documentId={this.state.id}
+          renderComment={this.state.renderComment}
+          renderLexicon={this.state.renderLexicon}
+          renderSearch={this.state.renderSearch}
+        />
+      </div>
+    );
+  }
 }
 
 SegmentContainer.propTypes = {
