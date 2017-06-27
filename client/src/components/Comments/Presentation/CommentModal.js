c1507fa97d67eca78716ee2d82bfd9055eaa3606
Daniel Turner <dturner@tcd.ie>
2017-06-27 15:13:38 +0100

    fix comment modal, still need to extend the overlay section to the full length

--- client/src/components/Comments/Presentation/CommentModal.js
+++ client/src/components/Comments/Presentation/CommentModal.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactModal from 'react-modal';
 
 import styles from '../styles.css';
+import main from '../../../constants/main.css';
 import { addComment } from '../../Comments/ActionCreators/CommentActions';
 import store from '../../../store';
 
@@ -17,26 +19,55 @@ export default class CommentModal extends React.Component {
 
   render() {
     return (
-      <div>
-        <div>Add Comment</div>
-        <form onSubmit={event => this.addComment(event, this.props.index)}>
-          <input className={styles.commentInput} autoComplete="false"
-            aria-label={`Comment input for segment ${this.props.index}`}
-          />
-          <div className={styles.commentButtons} >
-            <button className={styles.commentButton} type="submit">Add</button>
-            <button className={styles.commentButton}
-              onClick={() => this.props.unrender(this.props.index)}>Cancel
-            </button>
-          </div>
-        </form>
-      </div>
+      <ReactModal
+        isOpen={this.props.render}
+        contentLabel="Add Comment Modal"
+        overlayClassName={main.overlay}
+        shouldCloseOnOverlayClick
+        onRequestClose={this.props.handleCloseModal}
+        role="dialog"
+        style={{ content: {
+          top: this.props.ycoord,
+          left: this.props.xcoord,
+          position: 'absolute',
+          border: '1px solid #ccc',
+          background: '#fff',
+          outline: 'none',
+          padding: '20px',
+          width: '250px',
+          height: '100px',
+          zIndex: '101',
+        } }}>
+        <div>
+          <div>Add Comment</div>
+          <form onSubmit={event => this.addComment(event, this.props.index)}>
+            <input className={styles.commentInput} autoComplete="false"
+              aria-label={`Comment input for segment ${this.props.index}`}
+            />
+            <div className={styles.commentButtons} >
+              <button className={styles.commentButton} type="submit">Add</button>
+              <button className={styles.commentButton}
+                onClick={() => this.props.unrender(this.props.index)}>Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      </ReactModal>
     );
   }
 }
+
+CommentModal.defaultProps = {
+  xcoord: 0,
+  ycoord: 0,
+};
 
 CommentModal.propTypes = {
   documentId: PropTypes.number.isRequired,
   index: PropTypes.number.isRequired,
   unrender: PropTypes.func.isRequired,
+  render: PropTypes.bool.isRequired,
+  handleCloseModal: PropTypes.func.isRequired,
+  xcoord: PropTypes.number,
+  ycoord: PropTypes.number,
 };
