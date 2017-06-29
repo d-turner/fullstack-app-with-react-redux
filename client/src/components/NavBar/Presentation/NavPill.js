f39cb69a581fe5ecd7bc80f26531343ff12c4455
Daniel Turner <dturner@tcd.ie>
2017-06-29 09:55:25 +0100

    add some control for setting the currently selected tab

--- client/src/components/NavBar/Presentation/NavPill.js
+++ client/src/components/NavBar/Presentation/NavPill.js
@@ -2,18 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import styles from '../navbar.scss';
+
 const NavPill = (props) => {
+  const location =
+  (window.location.pathname === props.to ||
+  `/${window.location.pathname.split('/')[1]}` === props.to);
+  if (props.children) {
+    return (
+      <Link to={props.to} className={location ? `img ${styles.selected}` : 'img'} >
+        {props.children}
+      </Link>
+    );
+  }
   return (
-    <Link to={props.to}>
-      <div className={props.className}>{props.label}</div>
+    <Link to={props.to} className={location ? styles.selected : ''}>
+      <div>{props.label}</div>
     </Link>
   );
 };
 
+NavPill.defaultProps = {
+  children: undefined,
+};
+
 NavPill.propTypes = {
   to: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  children: PropTypes.element,
 };
 
 export default NavPill;
