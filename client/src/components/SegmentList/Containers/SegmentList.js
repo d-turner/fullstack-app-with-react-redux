84d56b7d924b3e52565240e6d2c376e7c972e7ec
Daniel Turner <dturner@tcd.ie>
2017-07-03 10:09:09 +0100

    Refactor constants within the render function to become class functions outside the render function

--- client/src/components/SegmentList/Containers/SegmentList.js
+++ client/src/components/SegmentList/Containers/SegmentList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactToolTip from 'react-tooltip';
 
-import styles from '../styles.css';
+import styles from '../segmentList.scss';
 import Segment from '../../Segment/Containers/Segment';
 import CommentModal from '../../Comments/Presentation/CommentModal';
 
@@ -12,7 +12,9 @@ class SegmentList extends React.Component {
     this.props.updateSelectedSegment(this.props.documentId, 0);
     const renderArray = new Array(props.segments.length).fill(false);
     this.state = { renderArray };
-    this.handleCloseModal = this.handleCloseModal.bind(this);
+
+    this.renderSegment = this.renderSegment.bind(this);
+    this.renderSingle = this.renderSingle.bind(this);
     this.unrender = this.unrender.bind(this);
   }
 
@@ -25,102 +27,98 @@ class SegmentList extends React.Component {
     this.setState({ renderArray: new Array(this.props.segments.length).fill(false) });
   }
 
-  handleCloseModal() {
-    this.setState({ renderArray: new Array(this.props.segments.length).fill(false) });
-  }
-
   renderComment(event, index) {
-    const xcoord = event.currentTarget.offsetLeft - 285;
-    const ycoord = event.currentTarget.offsetTop - 135;
     const newArray = this.state.renderArray.map((value, i) => {
       if (index === i) return true;
       return false;
     });
-    this.setState({ renderArray: newArray, xcoord, ycoord });
+    this.setState({ renderArray: newArray });
     this.props.renderComment();
   }
 
   renderSidebar(index) {
     return (
-      <div className={`${styles['sidebar-handle']}`}>
-        <div className={`${styles['sidebar-fixed']}`}>
-          <button className={`${styles['sidebar-wrapper']} ${styles['sidebar-button']}`}
-            data-tip data-for="Comment"
-            aria-label="Add a Comment"
-            onClick={event => this.renderComment(event, index)}>
-            <i className="tiny material-icons">chat_bubble</i>
-          </button>
-          <ReactToolTip id="Comment">
-            <span>Comments</span>
-          </ReactToolTip>
-          <CommentModal
-            documentId={this.props.documentId}
-            index={index}
-            unrender={this.unrender}
-            xcoord={this.state.xcoord} ycoord={this.state.ycoord}
-            handleCloseModal={this.handleCloseModal}
-            render={this.state.renderArray[index]} />
-          <button className={`${styles['sidebar-wrapper']} ${styles['sidebar-button']}`}
-            data-tip data-for="Lexicon"
-            onClick={() => this.props.renderLexicon()}
-            aria-label="Open Lexicon Sidebar">
-            <i className="small material-icons">translate</i>
-          </button>
-          <ReactToolTip id="Lexicon">
-            <span>Lexicon</span>
-          </ReactToolTip>
-          <button className={`${styles['sidebar-wrapper']} ${styles['sidebar-button']}`}
-            data-tip data-for="Search"
-            onClick={() => this.props.renderSearch()}
-            aria-label="Open Find and Replace ">
-            <i className="small material-icons">search</i>
-          </button>
-          <ReactToolTip id="Search">
-            <span>Search</span>
-          </ReactToolTip>
-        </div>
+      <div className={`id ${styles.clearPadding} ${styles.fixedSmallWidth}`}>
+        <button className={`${styles.clearButton} ${styles.buttonMargin}`}
+          data-tip data-for="Comment"
+          aria-label="Add a Comment"
+          onClick={event => this.renderComment(event, index)}>
+          <i className={`small material-icons ${styles.fixFont}`}>chat_bubble</i>
+        </button>
+        <ReactToolTip id="Comment">
+          <span>Comments</span>
+        </ReactToolTip>
+        <button className={`${styles.clearButton} ${styles.buttonMargin}`}
+          data-tip data-for="Lexicon"
+          onClick={() => this.props.renderLexicon()}
+          aria-label="Open Lexicon Sidebar">
+          <i className={`small material-icons ${styles.fixFont}`}>translate</i>
+        </button>
+        <ReactToolTip id="Lexicon">
+          <span>Lexicon</span>
+        </ReactToolTip>
+        <button className={`${styles.clearButton} ${styles.buttonMargin}`}
+          data-tip data-for="Search"
+          onClick={() => this.props.renderSearch()}
+          aria-label="Open Find and Replace ">
+          <i className={`small material-icons ${styles.fixFont} ${styles.buttonPadding}`}>search</i>
+        </button>
+        <ReactToolTip id="Search">
+          <span>Search</span>
+        </ReactToolTip>
+      </div>
+    );
+  }
+  renderSegment(segment, index) {
+    return (
+      <div className={`flex six ${styles.groupItem} ${styles.content}`} key={index} value={index}>
+        { index === this.props.selectedSegment ?
+          this.renderSelected(index) :
+          this.renderButton(index)
+        }
+        {this.renderSidebar(index)}
+        <CommentModal
+          documentId={this.props.documentId}
+          index={index}
+          unrender={this.unrender}
+          render={this.state.renderArray[index]} />
+      </div>
+    );
+  }
+
+  renderSelected(index) {
+    return (
+      <div className={`two-fifth ${styles.paddingInlineWithNavRight} ${styles.selected}`}>
+        {this.renderSingle(index)}
+      </div>
+    );
+  }
+
+  renderButton(index) {
+    return (
+      <button
+        onClick={() => this.selected(index)}
+        className={`two-fifth ${styles.block} ${styles.clearMarginTop}`}
+        aria-label="Activate the selected segment"
+        role={'textbox'}>
+        {this.renderSingle(index)}
+      </button>
+    );
+  }
+
+  renderSingle(index) {
+    return (
+      <div> {/* another wrapper for row flex*/}
+        <div className={styles.padNumber}>{index}</div>{/* segment number*/}
+        <Segment documentId={this.props.documentId} segmentId={index} editorState={this.props.editorState} />
       </div>
     );
   }
 
   render() {
-    const renderSegment = (segment, index) => {
-      if (index === this.props.selectedSegment) {
-        return (
-          <div key={index} value={index} className={styles.wrap}>
-            <div className={`${styles.selectedBlock} ${styles.selected} ${styles.segmentFlex}`}>
-              <div className={styles.segmentWrapper}> {/* another wrapper for row flex*/}
-                <div className={styles.segmentId}>{index}</div>{/* segment number*/}
-                <Segment documentId={this.props.documentId} segmentId={index} editorState={this.props.editorState} />
-              </div>
-            </div>
-            {this.renderSidebar(index)}
-          </div>
-        );
-      }
-
-      return (
-        <div className={styles.segmentFlex} key={index} value={index}>
-          <button
-            onClick={() => this.selected(index)}
-            className={styles.block}
-            aria-label="Activate the selected segment"
-            role={'textbox'}>
-            <div className={styles.segmentWrapper}> {/* another wrapper for row flex*/}
-              <div className={styles.segmentId}>{index}</div>{/* segment number*/}
-              <Segment documentId={this.props.documentId} segmentId={index} editorState={this.props.editorState} />
-            </div>
-          </button>
-          {this.renderSidebar(index)}
-        </div>
-      );
-    };
-
     return (
-      <div className={styles.segmentList}>
-        <div className={styles.wrapper}>
-          {this.props.segments.map(renderSegment)}
-        </div>
+      <div className={`four-fifth ${styles.centerContent}`}>
+        {this.props.segments.map(this.renderSegment)}
       </div>
     );
   }
