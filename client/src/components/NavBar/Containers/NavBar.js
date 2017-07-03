804fa3ed62f7feab81033aaee984a5822d1ca9df
Daniel Turner <dturner@tcd.ie>
2017-07-03 14:59:25 +0100

    remove unused tabs

--- client/src/components/NavBar/Containers/NavBar.js
+++ client/src/components/NavBar/Containers/NavBar.js
@@ -9,10 +9,7 @@ const NavBar = () => {
       <NavPill to={'/'} label={'Kanjingo'} >
         <img src={'/assets/kanj.png'} alt="kanjingo logo" className="img-responsive" />
       </NavPill>
-      <NavPill to={'/other'} label={'Other'} />
-      <NavPill to={'/old-other'} label={'Other Redirect'} />
-      <NavPill to={'/view/123'} label={'View 123'} />
-      <NavPill to={'/segments'} label={'Segments'} />
+
       <NavPill to={'/projects'} label={'Projects'} />
       <NavPill to={'/documents'} label={'Documents'} />
     </nav>
