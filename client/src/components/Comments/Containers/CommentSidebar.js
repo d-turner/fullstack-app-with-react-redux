831231de3e9671afaa7e80a8e70434ba254244cb
Daniel Turner <dturner@tcd.ie>
2017-07-04 13:50:06 +0100

    height fix, sidebar component handles the height of the viewport

--- client/src/components/Comments/Containers/CommentSidebar.js
+++ client/src/components/Comments/Containers/CommentSidebar.js
@@ -7,7 +7,7 @@ import CommentList from './CommentList';
 class CommentSidebar extends React.Component {
   render() {
     return (
-      <div className="flex one">
+      <div style={{ height: '100%' }}>
         <h3 className={styles.clearPaddingLeft}>Comments</h3>
         <div className={`${styles.overflow} ${styles.groupItem}`}>
           <div className={styles.flexItem}>
