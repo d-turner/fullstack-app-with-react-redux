56a3657d716d59588d0399c07b077ede167b04ad
Daniel Turner <dturner@tcd.ie>
2017-07-11 10:42:00 +0100

    add register, logout and login api calls to api wrapper

--- client/src/utils/apiWrapper.js
+++ client/src/utils/apiWrapper.js
@@ -8,19 +8,20 @@ const LOGOUT = '/api/logout';
 // prod: const API_HOSTNAME = 'http://kanjingo.adaptcentre.ie';
 const API_HOSTNAME = 'http://localhost:8080';
 
-export function registerUser(data, callback) {
+export function loginUser(data, callback) {
   /*
   * Expects Data to be:
-  * { name, email, password }
+  * { email, password }
   * Responses:
-  * { error: 'email already exists' }
+  * { error: 'Email or password is invalid' }
   * { error: 'not a valid X' }
-  * { success: 'user registered' }
+  * { success: 'Login successful!' }
   * { failed: 'something went wrong' }
   */
   axios({
     method: 'post',
     url: `${API_HOSTNAME}${LOGIN}`,
+    withCredentials: true,
     data,
   })
   .catch((error) => {
@@ -42,9 +43,80 @@ export function registerUser(data, callback) {
   });
 }
 
+export function registerUser(data, callback) {
+  /*
+  * Expects Data to be:
+  * { name, email, password }
+  * Responses:
+  * { error: 'email already exists' }
+  * { error: 'not a valid X' }
+  * { success: 'user registered' }
+  * { failed: 'something went wrong' }
+  */
+  axios({
+    method: 'post',
+    url: `${API_HOSTNAME}${REGISTER}`,
+    withCredentials: true,
+    data,
+  })
+  .catch((error) => {
+    if (error.response) {
+      // status code outside 200
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // no response received
+      console.log(error.request);
+    } else {
+      // config error
+      console.log('Error', error.message);
+    }
+  })
+  .then((response) => {
+    callback(response);
+  });
+}
+
+export function logoutUser(callback) {
+  /*
+  * Responses:
+  * { success: 'user logged out' }
+  * { failed: 'something went wrong' }
+  */
+  axios({
+    method: 'get',
+    url: `${API_HOSTNAME}${LOGOUT}`,
+    withCredentials: true,
+  })
+  .catch((error) => {
+    if (error.response) {
+      // status code outside 200
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // no response received
+      console.log(error.request);
+    } else {
+      // config error
+      console.log('Error', error.message);
+    }
+  })
+  .then((response) => {
+    callback(response);
+  });
+}
+
 const api = {
+  login: (data, callback) => {
+    loginUser(data, callback);
+  },
   register: (data, callback) => {
     registerUser(data, callback);
+  },
+  logout: (callback) => {
+    logoutUser(callback);
   },
 };
 
