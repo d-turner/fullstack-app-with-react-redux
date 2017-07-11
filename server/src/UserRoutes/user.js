9d3c0b62fd596533a4ef8664c8f50b7ed49d8659
Daniel Turner <dturner@tcd.ie>
2017-07-11 10:36:33 +0100

    add register route and refactor authentication callback

--- server/src/UserRoutes/user.js
+++ server/src/UserRoutes/user.js
@@ -1,30 +1,36 @@
 import passport from '../config/passport';
+import logger from '../util/logger';
 import User from '../db/user';
+import * as resp from '../config/Responses';
+
+
+function authenticatedCallback(err, user, info, req, res, next) {
+  if (err) return next(err);
+  if (!user) {
+    return res.status(resp.noAuth).json(info);
+  }
+  return req.logIn(user, (error) => {
+    if (error) {
+      logger.error(`Error: ${error}`);
+      return res.status(resp.error).json(info);
+    }
+    return res.status(resp.good).json(info);
+  });
+}
 
 export default (app) => {
   // login routes
   app.get('/api/login', (req, res) => {
-    res.send('Login Page');
+    if (req.user) {
+      return res.status(resp.good).json(resp.loggedIn);
+    }
+    return res.status(resp.good).json(resp.loggedOut);
   });
 
-  app.post('/api/login', function (req, res, next) {
-    passport.authenticate('login', function (err, user, info) {
-      if (err) return next(err);
-      if (!user) {
-        return res.status(401).json({
-            err: info,
-        });
-      }
-      req.logIn(user, function (error) {
-        if (error) {
-          return res.status(500).json({
-            err: 'Could not log in user',
-          });
-        }
-        res.status(200).json({
-          status: 'Login successful!',
-        });
-      });
+  app.post('/api/login', (req, res, next) => {
+    // body: { email, password }
+    passport.authenticate('login', (err, user, info) => {
+      authenticatedCallback(err, user, info, req, res, next);
     })(req, res, next);
   });
 
@@ -34,24 +40,22 @@ export default (app) => {
   });
 
   // register routes
-  app.get('/api/registered', (req, res) => {
-    res.send('Registration Successful');
-  });
-
-  app.post('/api/register', (req, res) => {
+  app.post('/api/register', (req, res, next) => {
     // body: { name, email, password }
     const data = req.body;
     if (!data.name && !data.email && !data.password) {
-      res.status().send({ error: 'Request is not valid, missing parameters' });
+      // Unprocessable Entity
+      res.status(resp.unprocessable).json(resp.badParameters);
     }
-    res.redirect('/api/login');
+    passport.authenticate('register', (err, user, info) => {
+      authenticatedCallback(err, user, info, req, res, next);
+    })(req, res, next);
   });
 
   // test routes
   app.get('/api/test', (req, res) => {
     passport.ensureAuthenticated(req, res, (status, reply) => {
-      res.status(status).send(reply);
+      res.status(status).json(reply);
     });
   });
 };
-
