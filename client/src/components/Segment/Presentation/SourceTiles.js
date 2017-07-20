c92b0c6b867dbca826e6d218941e71ee813f5239
Daniel Turner <dturner@tcd.ie>
2017-07-20 16:14:56 +0100

    add drag and drop functionality, works on mobile and desktop. TODO: find a way to get the order of the selected words

---
+++ client/src/components/Segment/Presentation/SourceTiles.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { DragSource } from 'react-dnd';
+import styles from '../segment.scss';
+
+const Types = {
+  TILE: 'tile',
+};
+
+const tileSource = {
+  beginDrag(props) {
+    // Return the data describing the dragged item
+    const item = { word: props.source, index: props.index };
+    
+    let data = '';
+    const wrapper = document.getElementById('wordTiles');
+    const tiles = wrapper.childNodes;
+    for (let i = 1; i < tiles.length; i++) {
+      const tile = tiles[i];
+      const input = tile.getElementsByTagName('input')[0];
+      const label = tile.getElementsByTagName('label')[0];
+      const span = label.getElementsByTagName('span')[0];
+      if (input.checked) {
+        data += span.innerText + ' ';
+      }
+    }
+    if (data !== '') {
+      item.word = data;
+    }
+
+    return item;
+  },
+
+};
+
+function collect(connect, monitor) {
+  return {
+    // Call this function inside render()
+    // to let React DnD handle the drag events:
+    connectDragSource: connect.dragSource(),
+    // preview of the dragging component
+    connectDragPreview: connect.dragPreview(),
+    // You can ask the monitor about the current drag state:
+    isDragging: monitor.isDragging(),
+  };
+}
+
+class SourceTiles extends React.Component {
+  render() {
+    const { connectDragSource, connectDragPreview, isDragging, source, index } = this.props;
+    return connectDragSource(
+      <div className={styles.inlineBlock}>
+        <input aria-label="select word for dragging" type="checkbox" id={`drag${source}${index}`} className={styles.check} />
+        <label className={styles.format} htmlFor={`drag${source}${index}`}
+          style={{
+            opacity: isDragging ? 0.5 : 1,
+            cursor: 'move' }}>
+          <span>{source}</span>
+          {connectDragPreview(<div style={{
+            width: '0px',
+            height: '0px',
+          }} />)}
+        </label>
+      </div>,
+    );
+  }
+}
+
+SourceTiles.propTypes = {
+  source: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  connectDragSource: PropTypes.func.isRequired,
+  connectDragPreview: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired,
+};
+
+export default DragSource(Types.TILE, tileSource, collect)(SourceTiles);
