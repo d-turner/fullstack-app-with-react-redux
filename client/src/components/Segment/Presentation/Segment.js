f5e8bb2c178256b8a4378548c2be8a63e000030e
Daniel Turner <dturner@tcd.ie>
2017-07-20 16:11:26 +0100

    removed old function and extracted all chaining stuff to it's own component

--- client/src/components/Segment/Presentation/Segment.js
+++ client/src/components/Segment/Presentation/Segment.js
@@ -5,59 +5,6 @@ import styles from '../segment.scss';
 import CustomEditor from '../../Editor/CustomEditor';
 
 class Segment extends React.Component {
-  dragStartHandler(event) {
-    let data = '';
-    const wrapper = document.getElementById('wordTiles1');
-    const tiles = wrapper.getElementsByTagName('div');
-    for (let i = 0; i < tiles.length; i++) {
-      const tile = tiles[i];
-      const input = tile.getElementsByTagName('input')[0];
-      const label = tile.getElementsByTagName('label')[0];
-      const span = label.getElementsByTagName('span')[0];
-      if (input.checked) {
-        data += span.innerText + ' ';
-      }
-    }
-    if (data === '') {
-      data = event.target.innerText;
-    }
-    event.dataTransfer.setData('text/plain', data);
-  }
-
-  dragEndHandler(event) {
-    event.preventDefault();
-    console.log(event);
-  }
-
-  allowDrop(event) {
-    event.preventDefault();
-   // console.log('Drop: ', event);
-  }
-
-  renderTile(word, index) {
-    return (
-      <div className={styles.inlineBlock}>
-        <input aria-label="select word for dragging" type="checkbox" name="rGroup" value="1" id={`drag${word}${index}`} className={styles.check} />
-        <label className={styles.format} htmlFor={`drag${word}${index}`} >
-          <span
-            draggable
-            onDragStart={event => this.dragStartHandler(event)}
-            onDragEnd={event => this.dragEndHandler(event)}
-            onTouchStart={event => this.dragStartHandler(event)}
-            onTouchMove={event => this.dragEndHandler(event)}
-            onTouchEnd={event => this.dragEndHandler(event)}>
-            {word}
-          </span>
-        </label>
-      </div>
-    );
-  }
-
-  dropHandler(event) {
-    event.preventDefault();
-    console.log('drop:', event);
-    console.log('drop:', event.target);
-  }
 
   renderEditor(selected) {
     if (selected) {
@@ -100,9 +47,6 @@ class Segment extends React.Component {
         <div className={selected ? `${styles.wrapper} ${styles.selected}` : `${styles.wrapper}`}>
           <h6>#{this.props.segmentId} Source</h6>
           {this.props.segment.source}
-          <div className={styles.format1} id={"wordTiles" + this.props.segmentId}>
-            {this.props.segment.source.split(' ').map((word, index) => this.renderTile(word, index))}
-          </div>
         </div>
         {this.renderEditor(selected)}
       </div>
