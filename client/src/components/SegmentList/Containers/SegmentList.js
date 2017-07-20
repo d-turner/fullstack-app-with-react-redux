4b41b955f6ce09b35aaa3b21b5864bd28c0b075c
Daniel Turner <dturner@tcd.ie>
2017-07-20 16:09:20 +0100

    add state to turn sentences into tiles

--- client/src/components/SegmentList/Containers/SegmentList.js
+++ client/src/components/SegmentList/Containers/SegmentList.js
@@ -11,7 +11,7 @@ class SegmentList extends React.Component {
   constructor(props) {
     super(props);
     const renderArray = new Array(props.segments.length).fill(false);
-    this.state = { renderArray };
+    this.state = { renderArray, renderTiles: false };
 
     this.renderSegment = this.renderSegment.bind(this);
     this.renderSingle = this.renderSingle.bind(this);
@@ -52,6 +52,10 @@ class SegmentList extends React.Component {
     this.props.renderComment();
   }
 
+  renderTiles(event, index) {
+    this.setState({ renderTiles: !this.state.renderTiles });
+  }
+
   renderSidebar(index) {
     return (
       <div className={`id ${styles.clearPadding} ${styles.fixedSmallWidth}`}>
@@ -88,7 +92,7 @@ class SegmentList extends React.Component {
         <button className={`${styles.clearButtonLeft} ${styles.buttonMargin} ${styles.button}`}
           data-tip data-for="Chain"
           aria-label="Chain Words"
-          onClick={event => this.renderComment(event, index)}>
+          onClick={event => this.renderTiles(event, index)}>
           <svg className={styles.chain} viewBox="0 0 24 24">
             <path fill="#333333" d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z" />
           </svg>
@@ -143,9 +147,11 @@ class SegmentList extends React.Component {
       return <div />;
     }
     return (
-      <div> {/* another wrapper for row flex*/}
-        <Segment documentId={this.props.documentId} segmentId={index} editorState={this.props.editorState} />
-      </div>
+      <Segment
+        documentId={this.props.documentId}
+        segmentId={index}
+        editorState={this.props.editorState}
+        renderTiles={this.state.renderTiles} />
     );
   }
 
