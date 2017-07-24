6e18d04eb5dcb325fb784220626b13c6ffacc13d
Daniel Turner <dturner@tcd.ie>
2017-07-24 17:46:51 +0100

    refactor buttons and use individual button function for render

--- client/src/components/ButtonList/index.js
+++ client/src/components/ButtonList/index.js
@@ -1,42 +1,70 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactToolTip from 'react-tooltip';
 
 import styles from '../../constants/main.scss';
 
-class ButtonList extends React.Component {
-  render() {
-    return (
-      <div className={`id ${styles.buttonList} ${styles.fixedPosition}`}>
-        <button className={`${styles.clearButton} ${styles.buttonMargin} ${styles.button}`}
-          data-tip data-for="CommentSide"
-          aria-label="Add a Comment"
-          onClick={() => this.props.renderComment()}>
-          <i className={`small material-icons ${styles.fixFont}`}>chat_bubble</i>
-        </button>
-        <ReactToolTip place="left" id="CommentSide" effect="solid">
-          <span>Comments</span>
-        </ReactToolTip>
-        <button className={`${styles.clearButton} ${styles.buttonMargin} ${styles.button}`}
-          data-tip data-for="Lexicon"
-          onClick={() => this.props.renderLexicon()}
-          aria-label="Open Lexicon Sidebar">
-          <i className={`small material-icons ${styles.fixFont}`}>translate</i>
-        </button>
-        <ReactToolTip place="left" id="Lexicon" effect="solid">
-          <span>Lexicon</span>
-        </ReactToolTip>
-        <button className={`${styles.clearButton} ${styles.buttonMargin} ${styles.button}`}
-          data-tip data-for="Search"
-          onClick={() => this.props.renderSearch()}
-          aria-label="Open Find and Replace ">
-          <i className={`small material-icons ${styles.fixFont} ${styles.buttonPadding}`}>search</i>
-        </button>
-        <ReactToolTip place="left" id="Search" effect="solid">
-          <span>Search</span>
-        </ReactToolTip>
-      </div>
-    );
-  }
+function ButtonList({ renderComment, renderLexicon, renderSearch }) {
+  const classNames = `${styles.clearButton} ${styles.buttonMargin} ${styles.button}`;
+  return (
+    <div className={`id ${styles.buttonList} ${styles.fixedPosition}`}>
+
+      <Button
+        classNames={classNames}
+        label="Add a Comment"
+        icon="chat_bubble"
+        func={renderComment}
+        id="Comments"
+      />
+
+      <Button
+        classNames={classNames}
+        label="Lexicon Sidebar"
+        icon="translate"
+        func={renderLexicon}
+        id="Lexicon"
+      />
+
+      <Button
+        classNames={classNames}
+        label="Find and Replace"
+        icon="search"
+        func={renderSearch}
+        id="Search"
+      />
+
+    </div>
+  );
 }
+
+ButtonList.propTypes = {
+  renderComment: PropTypes.func.isRequired,
+  renderLexicon: PropTypes.func.isRequired,
+  renderSearch: PropTypes.func.isRequired,
+};
+
+function Button({ classNames, label, icon, func, id }) {
+  return (
+    <button
+      className={classNames}
+      data-tip data-for={id}
+      aria-label={label}
+      onClick={() => func()}
+    >
+      <i className={`small material-icons ${styles.fixFont}`}>{icon}</i>
+      <ReactToolTip place="left" id={id} effect="solid">
+        <span>{id}</span>
+      </ReactToolTip>
+    </button>
+  );
+}
+
+Button.propTypes = {
+  classNames: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  func: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default ButtonList;
