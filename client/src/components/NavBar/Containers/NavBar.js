56a5ad10e2c4e0a4c7d1b0c50052d63c7068a8d4
Daniel Turner <dturner@tcd.ie>
2017-08-29 11:03:56 +0100

    add connect logic to NavBar and update to a class for NavPill

--- client/src/components/NavBar/Containers/NavBar.js
+++ client/src/components/NavBar/Containers/NavBar.js
@@ -1,21 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as actionCreators from '../../Authentication/AuthActions';
 import NavPill from '../Presentation/NavPill';
 
 require('../navbar.scss');
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <nav>
       <NavPill to={'/'} label={'Kanjingo'} >
         <img src={'/assets/kanj.png'} alt="kanjingo logo" className="img-responsive" />
       </NavPill>
-
-      <NavPill to={'/login'} label={'Login'} />
+      { props.isLoggedIn ?
+        <NavPill to={'/logout'} label={'Logout'} logout={props.logout} /> :
+        <NavPill to={'/login'} label={'Login'} />
+      }
       <NavPill to={'/projects'} label={'Projects'} />
       <NavPill to={'/documents'} label={'Documents'} />
     </nav>
   );
 };
 
+const mapStateToProps = function(state) {
+  // get the required reducer(s) from the state
+  const { authenticationReducer } = state;
+  // return what we want available in the props
+  const { userId, name, email, isLoggedIn } = authenticationReducer;
+  return {
+    userId, name, email, isLoggedIn,
+  };
+};
 
-export default NavBar;
+const mapDispatchToProps = function(dispatch) {
+  // get the available dispatch actions
+  return bindActionCreators(actionCreators, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
--- client/src/components/NavBar/Presentation/NavPill.js
+++ client/src/components/NavBar/Presentation/NavPill.js
@@ -3,24 +3,41 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import styles from '../navbar.scss';
+import store from '../../../store';
+import apiWrapper from '../../../utils/apiWrapper';
+import { logout } from '../../Authentication/AuthActions';
 
-const NavPill = (props) => {
-  const location =
-  (window.location.pathname === props.to ||
-  `/${window.location.pathname.split('/')[1]}` === props.to);
-  if (props.children) {
+export default class NavPill extends React.Component {
+  logoutUser() {
+    apiWrapper.logout((response) => {
+      console.log(response);
+    });
+    store.dispatch(logout());
+  }
+
+  render() {
+    const location =
+    (window.location.pathname === this.props.to ||
+    `/${window.location.pathname.split('/')[1]}` === this.props.to);
+    if (this.props.children) {
+      return (
+        <Link to={this.props.to} className={location ? `img ${styles.selected}` : 'img'} >
+          {this.props.children}
+        </Link>
+      );
+    }
+    if (this.props.label === 'Logout') {
+      return (
+        <button style={{ float: 'right', marginTop: '10px' }} onClick={() => this.logoutUser()}>Logout</button>
+      );
+    }
     return (
-      <Link to={props.to} className={location ? `img ${styles.selected}` : 'img'} >
-        {props.children}
+      <Link to={this.props.to} className={location ? styles.selected : ''}>
+        <div>{this.props.label}</div>
       </Link>
     );
   }
-  return (
-    <Link to={props.to} className={location ? styles.selected : ''}>
-      <div>{props.label}</div>
-    </Link>
-  );
-};
+}
 
 NavPill.defaultProps = {
   children: undefined,
@@ -31,5 +48,3 @@ NavPill.propTypes = {
   label: PropTypes.string.isRequired,
   children: PropTypes.element,
 };
-
-export default NavPill;
