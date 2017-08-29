458f138c96d07e86e7b5180c8662ad27c584f24a
Daniel Turner <dturner@tcd.ie>
2017-08-29 10:59:07 +0100

    authentication reducer

--- client/src/rootReducer/index.js
+++ client/src/rootReducer/index.js
@@ -4,12 +4,14 @@ import projectReducer from '../components/Project/Reducers/ProjectReducer';
 // import segmentReducer from '../components/Segment/Reducers/SegmentReducer';
 import documentReducer from '../components/Document/Reducers/DocumentReducer';
 import commentReducer from '../components/Comments/Reducers/CommentReducer';
+import authenticationReducer from '../components/Authentication/AuthReducer';
 
 // Reducer Root
 const reducerRoot = combineReducers({
   projectReducer,
   documentReducer,
   commentReducer,
+  authenticationReducer,
 });
 
 export default reducerRoot;
