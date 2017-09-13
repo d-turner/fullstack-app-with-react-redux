209affa9d06108a70ca18f672d2c3ff5c1cc0fe7
Daniel Turner <dturner@tcd.ie>
2017-09-13 13:53:58 +0100

    add build script and add to crontab

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+repos=(
+  "/home/dturner/git/kanjingo-react-redux"
+)
+
+echo ""
+echo "Pulling latest from git"
+
+for repo in "${repos[@]}"
+do
+  echo ""
+  echo "***** Getting latest for" ${repo} "*****"
+  cd "${repo}"
+  git fetch
+  git diff origin/master --quiet
+  if [ $? -eq 1 ]
+  then
+    echo "Need to Update..."
+    echo "Pulling from GIT..."
+    git pull
+    echo "Restarting PM2"
+    pm2 kill
+    echo "Starting Server"
+    cd server
+    npm install
+    npm run prod
+    echo "Server Started"
+    cd ..
+    echo "Starting Client"
+    cd client
+    npm install
+    npm run start-prod
+    echo "Client Started"
+  fi
+  echo "***** DONE *****"
+
+done
