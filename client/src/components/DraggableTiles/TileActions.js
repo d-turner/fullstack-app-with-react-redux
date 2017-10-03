be1cff45c3aaa5b109152b2e5bbb983caf58e046
Daniel Turner <dturner@tcd.ie>
2017-10-03 17:24:17 +0100

    add indexArr to action to keep track of which tiles are getting dragged

--- client/src/components/DraggableTiles/TileActions.js
+++ client/src/components/DraggableTiles/TileActions.js
@@ -1,13 +1,14 @@
 import * as actions from '../../constants/actionTypes';
 
-export function insertWord(dragIndex, hoverIndex, word, isBefore, segmentId, documentId) {
+export function insertWord(dragIndex, hoverIndex, word, targetWord, segmentId, documentId, indexArr) {
   return {
     type: actions.INSERT_WORD,
     dragIndex,
     hoverIndex,
     word,
-    isBefore,
+    targetWord,
     segmentId,
     documentId,
+    indexArr,
   };
 }
