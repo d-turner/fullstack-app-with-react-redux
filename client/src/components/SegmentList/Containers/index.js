f8ebb347decdefc403746411b2bdfb0edbab1ae4
Daniel Turner <dturner@tcd.ie>
2017-10-17 17:58:58 +0100

    add userId and email to props for saving

--- client/src/components/SegmentList/Containers/index.js
+++ client/src/components/SegmentList/Containers/index.js
@@ -6,11 +6,12 @@ import * as actionCreators from '../ActionCreators/SegmentListActions';
 
 const mapStateToProps = function(state) {
   // get the required reducer(s) from the state
-  const { documentReducer } = state;
+  const { documentReducer, authenticationReducer } = state;
   // return what we want available in the props
   const { documents, editorState, selectedSegment } = documentReducer;
+  const { email, userId } = authenticationReducer;
   return {
-    documents, editorState, selectedSegment,
+    documents, editorState, selectedSegment, email, userId,
   };
 };
 
