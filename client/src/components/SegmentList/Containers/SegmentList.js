8f679bc4e44bd0ffffc5904bcc9b063e0ddf9928
Daniel Turner <dturner@tcd.ie>
2017-11-22 14:25:47 +0000

    update icons and use different imports

--- client/src/components/SegmentList/Containers/SegmentList.js
+++ client/src/components/SegmentList/Containers/SegmentList.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import ReactToolTip from 'react-tooltip';
 
 import styles from '../segmentList.scss';
+import main from '../../../constants/main.scss';
 import Segment from '../../Segment/Containers/Segment';
 import PlainSegment from '../../Segment/Presentation/PlainSegment';
 import CommentModal from '../../Comments/Presentation/CommentModal';
@@ -14,7 +15,7 @@ class SegmentList extends React.Component {
     super(props);
     const editArray = new Array(props.segments.length).fill(false);
     editArray[0] = true;
-    this.state = { editArray, renderComment: false, renderModal: false, renderTiles: false, renderVoice: false };
+    this.state = { editArray, renderComment: false, renderModal: false, renderTiles: false, renderVoice: false, help: true};
 
     this.renderSegment = this.renderSegment.bind(this);
     this.renderSingle = this.renderSingle.bind(this);
@@ -67,8 +68,8 @@ class SegmentList extends React.Component {
 
   renderSidebar(index) {
     return (
-      <div className={`id ${styles.clearPadding} ${styles.fixedSmallWidth}`}>
-        <button className={`${styles.clearButtonLeft} ${styles.buttonMargin} ${styles.button}`}
+      <div className={`id ${styles.clearPadding} ${main.fixedSmallWidth}`}>
+        <button className={`${main.clearButtonLeft} ${main.buttonMargin} ${styles.button}`}
           data-tip data-for="Comment"
           aria-label="Add a Comment"
           onClick={() => this.renderComment()}>
@@ -98,16 +99,14 @@ class SegmentList extends React.Component {
           <span>Merge With Next</span>
         </ReactToolTip>
         */ }
-        <button className={`${styles.clearButtonLeft} ${styles.buttonMargin} ${styles.button}`}
+        <button className={`${main.clearButtonLeft} ${main.buttonMargin} ${styles.button}`}
           data-tip data-for="Chain"
           aria-label="Chain Words"
           onClick={() => this.renderTiles()}>
-          <svg className={styles.chain} viewBox="0 0 24 24">
-            <path d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z" />
-          </svg>
+          <i className={`small material-icons ${styles.fixFont}`}>view_comfy</i>
         </button>
         <ReactToolTip place="right" id="Chain" effect="solid">
-          <span>Chain Words</span>
+          <span>Tile View</span>
         </ReactToolTip>
         { /*
         <button className={`${styles.clearButtonLeft} ${styles.buttonMargin} ${styles.button}`}
@@ -141,11 +140,28 @@ class SegmentList extends React.Component {
 
   renderSelected(index) {
     let responsive = 'three-fourth three-fifth-700 two-fifth-1400';
+    let message = 
+    "To activate voice input click the microphone icon and begin dictating\n\
+    You can edit the input once you are finished\n\
+    Select the position in the editor to insert the text and click the tick\n\
+    Click the X to clear the input";
     if (this.state.renderTiles) {
       responsive = 'three-fourth four-fifth-700 three-fifth-1400';
+      message = 
+      "Double tap a tile to edit and press 'Enter' to accept or 'Esc' to cancel.\n\
+      Tiles can be dragged from the source to the target or can be rearranged in the target.\n\
+      Multiple tiles can be dragged by first selecting and highlighting the desired tiles.";
     }
     return (
       <div className={`${responsive} ${styles.selected}`} id="selectedSegment">
+        {this.state.help ? (
+          <div style={{ padding: '10px 5px 21px 10px', marginRight: '2px'}} className={main.message}>
+            <span className={main.close}><i className="material-icons" onClick={() => this.setState({ help: false })}>close</i></span>
+            <span className={main.help}>
+            {message}
+            </span>
+          </div>
+        ) : (null) }
         {this.renderSingleSelected(index)}
         <CommentModal
           documentId={this.props.documentId}
@@ -224,6 +240,8 @@ SegmentList.propTypes = {
   selectedSegment: PropTypes.number.isRequired,
   mergeSegment: PropTypes.func.isRequired,
   splitSegment: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+  documents: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
 export default SegmentList;
