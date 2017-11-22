32adf2d0f94331b8daf24c16e955d02f0aad6400
Daniel Turner <dturner@tcd.ie>
2017-11-22 14:19:48 +0000

    add util for tokenizing string

--- client/src/utils/stringParser.js
+++ client/src/utils/stringParser.js
@@ -1,8 +1,15 @@
 export function splitTextIntoArray(text) {
-  let prep = text.replace(/\./g, ' .');
-  prep = prep.replace(/,/g, ' ,');
+  let prep = text.replace(/\./g, '.');
+  // prep = prep.replace(/,/g, ' ,');
   prep = prep.replace(/ +/g, ' ');
   const wordArray = prep.split(' ').filter((e) => { return e === 0 || e; });
+  // for (let i = 1; i < wordArray.length; i++) {
+  //   if (!isNaN(wordArray[i - 1]) && !isNaN(wordArray[i + 1]) && isNaN(wordArray[i])) {
+  //     wordArray[i - 1] = wordArray[i - 1] + wordArray[i] + wordArray[i + 1];
+  //     wordArray[i] = '';
+  //     wordArray[i + 1] = '';
+  //   }
+  // }
   return wordArray;
 }
 
@@ -14,3 +21,18 @@ export function joinTextArray(array) {
   .replace(/[ ]{1,}/g, ' ')
   .trim();
 }
+
+export function cleanText(text, lowercaseBefore) {
+  let newText = (text === undefined || text === null) ? '' : text;
+  if (lowercaseBefore) { newText = newText.toLowerCase(); }
+  newText = newText.replace(/\r?\n|\r/g, '');
+  newText = newText.replace(/\./g, '. ');
+  newText = newText.replace(/,/g, ', ');
+  newText = newText.replace(/ +/g, ' ');
+  newText = newText.replace(/(<([^>]+)>)/ig, '');
+  newText = newText.split('.').map(data => data.trim()).join('. ');
+  newText = newText.trim();
+  return newText.toString().replace(/(^|\. *)([a-z])/g, (match, separator, char) => {
+    return separator + char.toUpperCase();
+  });
+};
