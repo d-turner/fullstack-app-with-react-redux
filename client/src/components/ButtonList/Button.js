3363f544627f67266968f74d2e1dd117a6e33b04
Daniel Turner <dturner@tcd.ie>
2017-11-28 11:11:24 +0000

    refactored out a reusable button from the buttonList component

---
+++ client/src/components/ButtonList/Button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactToolTip from 'react-tooltip';
+
+import styles from '../../constants/main.scss';
+
+
+function Button({ classNames, label, icon, func, id, direction }) {
+  return (
+    <button
+      className={classNames}
+      data-tip data-for={id}
+      aria-label={label}
+      onClick={func}
+    >
+      <i className={`small material-icons ${styles.fixFont}`}>{icon}</i>
+      <ReactToolTip place={direction} id={id} effect="solid">
+        <span>{id}</span>
+      </ReactToolTip>
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  direction: 'left',
+};
+
+Button.propTypes = {
+  classNames: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  func: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  direction: PropTypes.string,
+};
+
+export default Button;
--- client/src/components/ButtonList/index.js
+++ client/src/components/ButtonList/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactToolTip from 'react-tooltip';
 
+import Button from './Button';
 import styles from '../../constants/main.scss';
 
 function ButtonList({ renderComment, renderLexicon, renderSearch }) {
@@ -41,30 +41,6 @@ ButtonList.propTypes = {
   renderComment: PropTypes.func.isRequired,
   renderLexicon: PropTypes.func.isRequired,
   renderSearch: PropTypes.func.isRequired,
-};
-
-function Button({ classNames, label, icon, func, id }) {
-  return (
-    <button
-      className={classNames}
-      data-tip data-for={id}
-      aria-label={label}
-      onClick={() => func()}
-    >
-      <i className={`small material-icons ${styles.fixFont}`}>{icon}</i>
-      <ReactToolTip place="left" id={id} effect="solid">
-        <span>{id}</span>
-      </ReactToolTip>
-    </button>
-  );
-}
-
-Button.propTypes = {
-  classNames: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
-  func: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
 };
 
 export default ButtonList;
