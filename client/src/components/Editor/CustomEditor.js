f2526ddc710772693c391c71ac1cf03ad644457e
Daniel Turner <dturner@tcd.ie>
2017-12-01 13:54:42 +0000

    temporary adding editor for tracking

--- client/src/components/Editor/CustomEditor.js
+++ client/src/components/Editor/CustomEditor.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Editor, EditorState, getDefaultKeyBinding, Modifier } from 'draft-js';
+import ReactToolTip from 'react-tooltip';
 
+import Button from '../ButtonList/Button';
 import VoiceInput from '../VoiceInputModal/react-speech-recognition-input';
 import BlockStyleControls from '../Editor/BlockStyleControls';
 import InlineStyleControls from '../Editor/InlineStyleControls';
 import styles from './Editor.scss';
+import main from '../../constants/main.scss';
 
 class CustomEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasFocus: false, renderStyles: false, renderVoice: false };
+  }
+
   componentDidMount() {
     this.Editor.focus();
     this.insertIntoEditor = this.insertIntoEditor.bind(this);
@@ -48,40 +56,45 @@ class CustomEditor extends React.Component {
   }
 
   render() {
+    let style = main['underline-from-center'];
+    if (this.state.hasFocus) {
+      style = `${styles.focus} ${main['underline-center']} ${main.animate}`;
+    }
     return (
       <div className={styles['RichEditor-root']} >
-        <h6>Target</h6>
-        <div className={styles['RichEditor-editor']} >
+        {this.state.renderStyles ?
+          <Styles
+            editorState={this.props.editorState}
+            toggleBlockType={this.props.toggleBlockType}
+            toggleInlineStyle={this.props.toggleInlineStyle} /> :
+          null
+        }
+        {/*<div style={{ marginBottom: '-55px' }}>
+          <Button
+            classNames={`${styles.styles} ${main.button}`}
+            label="styles" icon="format_paint" direction={'right'}
+            func={() => this.setState({ renderStyles: !this.state.renderStyles, renderVoice: false })}
+            id="Add Style" />
+        </div>
+        <div style={{ marginBottom: '-50px' }}>
+          <Button
+            classNames={`${styles.voice} ${main.button}`}
+            label="styles" icon="settings_voice" direction={'right'}
+            func={() => this.setState({ renderStyles: false, renderVoice: !this.state.renderVoice })}
+            id="Open Voice Dialogue" />
+      </div>*/}
+        <div className={`${styles['RichEditor-editor']} ${style}`} >
           <Editor
             editorState={this.props.editorState}
             handleKeyCommand={this.props.handleKeyCommand}
             keyBindingFn={this.myKeyBindingFn}
+            onFocus={() => this.setState({ hasFocus: true })}
+            onBlur={() => this.setState({ hasFocus: false })}
             onChange={this.props.handleChange}
             ref={(ref) => { this.Editor = ref; }}
             aria-label="Translation Input"
           />
         </div>
-        <details>
-          <summary className={styles.summary}>Styles</summary>
-          <BlockStyleControls
-            editorState={this.props.editorState}
-            onToggle={this.props.toggleBlockType}
-            className={styles['RichEditor-styleButton']}
-            activeClass={styles['RichEditor-activeButton']}
-          />
-          <InlineStyleControls
-            editorState={this.props.editorState}
-            onToggle={this.props.toggleInlineStyle}
-            className={styles['RichEditor-styleButton']}
-            activeClass={styles['RichEditor-activeButton']}
-        />
-        </details>
-        <VoiceInput
-          lang={this.props.lang}
-          editor={this.Editor}
-          onChange={value => this.insertIntoEditor(value)}
-          onEnd={endValue => this.endValue(endValue)}
-        />
       </div>
     );
   }
@@ -103,3 +116,28 @@ CustomEditor.propTypes = {
 };
 
 export default CustomEditor;
+
+const Styles = function(props) {
+  return (
+    <div className={`${styles.styleBox} `}>
+      <BlockStyleControls
+        editorState={props.editorState}
+        onToggle={props.toggleBlockType}
+        className={styles['RichEditor-styleButton']}
+        activeClass={styles['RichEditor-activeButton']}
+      />
+      <InlineStyleControls
+        editorState={props.editorState}
+        onToggle={props.toggleInlineStyle}
+        className={styles['RichEditor-styleButton']}
+        activeClass={styles['RichEditor-activeButton']}
+      />
+    </div>
+  );
+};
+
+Styles.propTypes = {
+  editorState: PropTypes.objectOf(PropTypes.any).isRequired,
+  toggleBlockType: PropTypes.func.isRequired,
+  toggleInlineStyle: PropTypes.func.isRequired,
+};
