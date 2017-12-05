8959b93c0b76eb96aa06313db2c837c8a3825742
Daniel Turner <dturner@tcd.ie>
2017-12-05 11:45:46 +0000

    use sass variables and constant for flex grid

--- client/src/components/Authentication/AuthPresentation.js
+++ client/src/components/Authentication/AuthPresentation.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 import styles from './auth.scss';
 import main from '../../constants/main.scss';
 
-const outerWrapper = 'flex one two-600 three-1100 center';
-const innerWrapper = 'flex one third-1100 fourth-1400 center';
+const outerWrapper = 'flex one two-700 three-1200 center';
+const innerWrapper = '';
 
 export default function Auth(props) {
   return (
