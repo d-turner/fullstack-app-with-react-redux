b934fbe138ac57fcd0087422c8a7e91d8e8feb04
Daniel Turner <dturner@tcd.ie>
2018-01-04 16:59:55 +0000

    add reducer to handle deleting a document and add META key to document

--- client/src/components/Document/Reducers/DocumentListReducer.js
+++ client/src/components/Document/Reducers/DocumentListReducer.js
@@ -10,16 +10,41 @@ const blankDocument = {
   isFetching: false,
   didInvalidate: true,
   xliff: {},
+  meta: null,
+  segments: null,
 };
 
 const createNewDocumentEntry = (state = blankDocument, doc) => {
+  let meta = null;
+  if (doc.segment_count !== null) {
+    meta = {
+      completedSegments: doc.completed_segments,
+      segmentCount: doc.segment_count,
+      listOrder: doc.list_order,
+      totalWords: doc.total_word_count,
+    };
+  }
   return Object.assign({}, state, {
     id: doc.document_id,
     name: doc.name,
     saved_name: doc.saved_name,
     description: doc.description,
     created_at: doc.created_at,
+    xliff: doc.xliff || {},
+    segments: doc.segments || null,
+    didInvalidate: doc.didInvalidate !== undefined ? doc.didInvalidate : true,
+    isFetching: doc.isFetching !== undefined ? doc.isFetching : false,
+    meta,
   });
+};
+
+const deleteDocument = (state, documentId) => {
+  return Object.keys(state)
+    .filter(key => key !== documentId)
+    .reduce((result, current) => {
+      result[current] = state[current];
+      return result;
+    }, {});
 };
 
 export default function DocumentList(state, action) {
@@ -35,51 +60,12 @@ export default function DocumentList(state, action) {
         documents: { $merge: documents },
       });
     }
+    case actions.DELETE_DOCUMENT: {
+      return update(state, {
+        documents: { $set: deleteDocument(state.documents, action.documentId) },
+      });
+    }
     default:
       return state;
   }
 }
-
-// import { Map } from 'immutable';
-// import update from 'immutability-helper';
-
-// import * as actions from '../../../constants/actionTypes';
-
-// const blankDocument = Map({
-//   id: '',
-//   name: '',
-//   saved_name: '',
-//   description: '',
-//   created_at: '',
-//   isFetching: false,
-//   didInvalidate: true,
-//   xliff: {},
-// });
-
-// const createNewDocumentEntry = (state = blankDocument, doc) => {
-//   return state.merge({
-//     id: doc.document_id,
-//     name: doc.name,
-//     saved_name: doc.saved_name,
-//     description: doc.description,
-//     created_at: doc.created_at,
-//   });
-// };
-
-// export default function DocumentList(state, action) {
-//   switch (action.type) {
-//     case actions.INSERT_DOCUMENTS: {
-//       const documents = {};
-//       const docs = action.documents;
-//       for (let x = 0; x < docs.length; x++) {
-//         const savedName = docs[x].saved_name;
-//         documents[savedName] = createNewDocumentEntry(state.documents[savedName], docs[x]);
-//       }
-//       return update(state, {
-//         documents: { $merge: documents },
-//       });
-//     }
-//     default:
-//       return state;
-//   }
-// }
