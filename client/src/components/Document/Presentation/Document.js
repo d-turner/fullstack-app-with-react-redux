2d9cd3d1cbaf126ab5e5a2e893096daacb3be428
Daniel Turner <dturner@tcd.ie>
2018-01-04 16:58:06 +0000

    add buttons for downloading and deleting documents

--- client/src/components/Document/Presentation/Document.js
+++ client/src/components/Document/Presentation/Document.js
@@ -2,49 +2,107 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import apiWrapper from '../../../utils/apiWrapper';
+import store from '../../../store';
+import { deleteDocument } from '../ActionCreators/DocumentActions';
+import Button from '../../ButtonList/Button';
+import main from '../../../constants/main.scss';
 import styles from '../documents.scss';
 
-export default function Document(props) {
-  const { id, documentName } = props;
-  if (props.document.error) {
-    return null;
-  }
-  return (
-    <tr className={styles.tableBorder}>
-      <td className={styles.tableRowHeight}>
-        <div>
-          <span>
-            <input type="radio"
-              aria-label="Select Document"
-              title="Select Document" />
-            <label htmlFor="checked_45986" />
+export default class Document extends React.Component {
+  state = { deleteConfirm: false };
+
+  render() {
+    const { id, document } = this.props;
+    if (this.props.document.error) {
+      return null;
+    }
+    return (
+      <tr className={styles.tableBorder}>
+        <td className={styles.tableRowHeight} data-key={id}>
+          <strong>{document.id}</strong>
+        </td>
+        <td className={styles.tableRowHeight}>
+          <Link to={`/documents/${id}`} className={styles.documentFilename}>{document.name}</Link>
+        </td>
+        <td>
+          <span >
+            <strong>{document.meta ? document.meta.segmentCount : null}</strong>
           </span>
-        </div>
-      </td>
-      <td>
-        <div>
-          <div>
-            <Link to={`/documents/${id}`} className={styles.documentFilename}>{documentName}</Link>
+        </td>
+        <td>
+          <span><strong>{document.meta ? document.meta.completedSegments : null}</strong></span>
+        </td>
+        <td>
+          <span><strong>{document.meta ? document.meta.totalWords : null}</strong></span>
+        </td>
+        <td>
+          {this.state.deleteConfirm ? (
+            <div className="flex three" id="check">
+              <Button
+                classNames={`${main.clearButtonLeft} ${main.button} ${styles.inherit} ${main.redButton}`}
+                label="Confirm"
+                icon="done"
+                func={() => store.dispatch(deleteDocument(document.id, id))}
+                id="Confirm"
+                tooltip={false} />
+              <div className={`off-third ${main.clearPadding}`}>
+                <Button
+                  classNames={`${main.clearButtonLeft} ${main.button} ${styles.inherit} ${main.greenButton}`}
+                  label="Cancel"
+                  icon="clear"
+                  func={() => this.setState({ deleteConfirm: false })}
+                  id="Cancel"
+                  tooltip={false} />
+              </div>
+            </div>
+          ) : (
+            <div id="remove">
+              <Button
+                classNames={`${main.clearButtonLeft} ${main.button} ${styles.inherit}`}
+                label="Delete document"
+                icon="delete"
+                func={() => this.setState({ deleteConfirm: true })}
+                id="Delete document"
+                tooltip={false} />
+            </div>
+          )}
+        </td>
+        <td>
+          <div id="download">
+            <Button
+              classNames={`${main.clearButtonLeft} ${main.downloadButton} ${styles.inherit}`}
+              label="Download document"
+              icon="file_download"
+              func={() => apiWrapper.getDocument(document.saved_name, (response) => {
+                // Create a new Blob object using the
+                // response data of the onload object
+                const blob = new Blob([response.data], { type: 'application/xml' });
+                // Create a link element, hide it, direct
+                // it towards the blob, and then 'click' it programatically
+                const a = window.document.createElement('a');
+                a.style = 'display: none';
+                window.document.body.appendChild(a);
+                // Create a DOMString representing the blob
+                // and point the link element towards it
+                const url = window.URL.createObjectURL(blob);
+                a.href = url;
+                a.download = document.name;
+                // programatically click the link to trigger the download
+                a.click();
+                // release the reference to the file by revoking the Object URL
+                window.URL.revokeObjectURL(url);
+              })}
+              id="download"
+              tooltip={false} />
           </div>
-        </div>
-      </td>
-      <td>
-        <span >
-          <strong>{props.document.xliff.segments ? props.document.xliff.segments.length : null}</strong>
-        </span>
-      </td>
-      <td>
-        <span><strong /></span>
-      </td>
-      <td>
-        <span><strong /></span>
-      </td>
-    </tr>
-  );
+        </td>
+      </tr>
+    );
+  }
 }
 
 Document.propTypes = {
   document: PropTypes.objectOf(PropTypes.any).isRequired,
   id: PropTypes.string.isRequired,
-  documentName: PropTypes.string.isRequired,
 };
