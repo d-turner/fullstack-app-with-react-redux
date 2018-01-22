9e125fe8da7233ece16ffc2a230a3faf47446372
Daniel Turner <dturner@tcd.ie>
2018-01-22 15:11:24 +0000

    fix import

--- client/src/components/Notifications/Alerts.js
+++ client/src/components/Notifications/Alerts.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import alert from './alert.scss';
-import { capitalizeFirstLetter } from '../../utils/stringParser';
+import { upperFirstLetter } from '../../utils/stringParser';
 
 const Alert = function({ message, type, state, onClick, index }) {
   if (!state) return null;
   if (message === null || message === undefined) return null;
-  const header = capitalizeFirstLetter(type);
+  const header = upperFirstLetter(type);
   return (
     <div role="status" className={`${alert.alert} ${alert[type]}`} onClick={() => onClick(index)}>
       <h4 className={alert.message}>{header}</h4>
