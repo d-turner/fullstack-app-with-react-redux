c323f9ddb50e3f0d1ee601f61376f2529cff36c0
Daniel Turner <dturner@tcd.ie>
2018-02-05 12:18:36 +0000

    use promise api

--- client/src/components/Dropzone/dropActions.js
+++ client/src/components/Dropzone/dropActions.js
@@ -24,7 +24,6 @@ export function setFail(response) {
 
 // build meta data from a document object
 export function buildMeta(doc, xliff, index = 0) {
-  console.log(doc);
   const documentId = doc.document_id;
   const segmentCount = xliff.segments.length;
   const listOrder = index;
@@ -66,38 +65,41 @@ export function buildMeta(doc, xliff, index = 0) {
   // dispatch this to the api
   return (dispatch) => {
     dispatch(setDocumentMeta());
-    apiWrapper.setDocumentMeta(documentId, documentMeta, (res) => {
-      console.log('Document META Response: ', res);
-      dispatch({
-        type: actions.INSERT_DOCUMENTS,
-        documents: [{
-          completed_segments: completedSegments,
-          created_at: doc.created_at,
-          description: doc.description,
-          document_id: documentId,
-          list_order: listOrder,
-          name: doc.name,
-          saved_name: doc.saved_name,
-          segment_count: segmentCount,
-          total_word_count: totalWords,
-          xliff,
-          didInvalidate: false,
-          isFetching: false,
-        }],
+    apiWrapper.setDocumentMeta(documentId, documentMeta)
+      .then((response) => {
+        dispatch({
+          type: actions.INSERT_DOCUMENTS,
+          documents: [{
+            completed_segments: completedSegments,
+            created_at: doc.created_at,
+            description: doc.description,
+            document_id: documentId,
+            list_order: listOrder,
+            name: doc.name,
+            saved_name: doc.saved_name,
+            segment_count: segmentCount,
+            total_word_count: totalWords,
+            xliff,
+            didInvalidate: false,
+            isFetching: false,
+          }],
+        });
+      })
+      .catch((error) => {
+        console.log('Build meta failed with error', error);
       });
-    });
-    for (let i = 0; i < segmentCount; i++) {
-      // apiWrapper.setSegment(documentId, segmentState[i], (response) => {
-      //   console.log('Segment ', i, ' Response: ', response);
-      //   // if (response && response.status === 200) {
-      //   //   dispatch(setSuccess(response.data));
-      //   // } else {
-      //   //   dispatch(setFail(response));
-      //   // }
-      //   return {
-      //     type: 'UPDATED_SEGMENT_META',
-      //   };
-      // });
-    }
+    // for (let i = 0; i < segmentCount; i++) {
+    //   // apiWrapper.setSegment(documentId, segmentState[i], (response) => {
+    //   //   console.log('Segment ', i, ' Response: ', response);
+    //   //   // if (response && response.status === 200) {
+    //   //   //   dispatch(setSuccess(response.data));
+    //   //   // } else {
+    //   //   //   dispatch(setFail(response));
+    //   //   // }
+    //   //   return {
+    //   //     type: 'UPDATED_SEGMENT_META',
+    //   //   };
+    //   // });
+    // }
   };
 }
