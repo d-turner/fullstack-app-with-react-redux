15f92023a998d88dae47067c24a8512e84014d64
Daniel Turner <dturner@tcd.ie>
2018-02-05 12:19:29 +0000

    remove unused action

--- client/src/components/DraggableTiles/TileActions.js
+++ client/src/components/DraggableTiles/TileActions.js
@@ -1,18 +1,5 @@
 import * as actions from '../../constants/actionTypes';
 
-export function insertWord(dragIndex, hoverIndex, word, targetWord, segmentId, documentId, indexArr) {
-  return {
-    type: actions.INSERT_WORD,
-    dragIndex,
-    hoverIndex,
-    word,
-    targetWord,
-    segmentId,
-    documentId,
-    indexArr,
-  };
-}
-
 export function insertSourceWord(index, word, isBefore, segmentId, documentId) {
   return {
     type: actions.INSERT_SOURCE_WORD,
