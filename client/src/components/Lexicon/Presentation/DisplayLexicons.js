5e453f7d97f39876fae2964ff6b17c8fb203c14a
Daniel Turner <dturner@tcd.ie>
2018-02-14 10:49:24 +0000

    add functional component to display a single result, add an iterator to run through all the results and find the most useful ones, move find lemma functions from container to here

--- client/src/components/Lexicon/Presentation/DisplayLexicons.js
+++ client/src/components/Lexicon/Presentation/DisplayLexicons.js
@@ -6,7 +6,7 @@ import main from '../../../constants/main.scss';
 
 function DisplayLexicon(props) {
   return (
-    <div style={{ height: '100%' }}>
+    <div className={main.maxHeight}>
       <h3 className={main.clearMarginLeft}>Lexicon</h3>
       <form className={styles.form} onSubmit={props.onSubmit}>
         <label htmlFor="searchCheckbox">
@@ -28,6 +28,15 @@ function DisplayLexicon(props) {
           autoFocus="" aria-autocomplete="list"
           aria-label="lexicon search input"
         />
+        <div style={{ textAlign: 'left', margin: '0 0 10px 0' }}>
+          <input type="submit"
+            text="Search"
+            style={{
+              background: 'white',
+              color: 'black',
+              border: 'solid 1px black',
+            }} />
+        </div>
       </form>
       <div className="flex two">
         <div className="half">
@@ -47,6 +56,66 @@ function DisplayLexicon(props) {
           </div>
         </div>
         {props.renderSpinner()}
+      </div>
+      <div className={styles.results}>
+        {props.result !== null ?
+          props.result.map((data, index) => RenderResult(props.sourceCode, props.targetCode, data, index))
+          : null}
+      </div>
+    </div>
+  );
+}
+
+// returns the sense of an array
+// alternative words
+function findFirstLemma(lang, array) {
+  for (let x = 0; x < array.length; x++) {
+    if (array[x].language === lang) return array[x].lemma.split('_').join(' ');
+  }
+  return null;
+}
+
+// returns the gloss with the most amount of tokens
+// gloss is the description of the word
+function findFirstGloss(lang, array) {
+  let tokenLength = 0;
+  let gloss = null;
+  for (let x = 0; x < array.length; x++) {
+    if (array[x].language === lang && array[x].tokens.length >= tokenLength) {
+      gloss = array[x];
+      tokenLength = array[x].tokens.length;
+    }
+  }
+  return gloss;
+}
+
+
+function RenderResult(sourceLang, targetLang, data, index) {
+  const sourceGloss = findFirstGloss(sourceLang, data.glosses);
+  const targetGloss = findFirstGloss(targetLang, data.glosses);
+  const sourceLemma = findFirstLemma(sourceLang, data.senses);
+  const targetLemma = findFirstLemma(targetLang, data.senses);
+  console.log(sourceLang, targetLang);
+  console.log(sourceGloss, sourceLemma, targetGloss, targetLemma);
+  return (
+    <div className="flex two" key={index}>
+      <div className="half">
+        {sourceGloss !== null ? (
+          <div>
+            <p>{sourceGloss.sourceSense.split('_').join(' ')}</p>
+            <p>{sourceLemma}</p>
+            <p>{sourceGloss.gloss}</p>
+          </div>
+        ) : null}
+      </div>
+      <div className="half">
+        {targetGloss !== null ? (
+          <div>
+            <p>{targetGloss.sourceSense.split('_').join(' ')}</p>
+            <p>{targetLemma}</p>
+            <p>{targetGloss.gloss}</p>
+          </div>
+        ) : null}
       </div>
     </div>
   );
