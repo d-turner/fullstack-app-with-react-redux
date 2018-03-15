b89ee5d630f4e20763c2846221d44882274d35d6
Daniel Turner <dturner@tcd.ie>
2018-03-15 12:11:25 +0000

    add route for accepting a string to tokenize

--- server/src/UserRoutes/segmentRoutes.js
+++ server/src/UserRoutes/segmentRoutes.js
@@ -1,7 +1,7 @@
 import passport from '../config/passport';
 import segment from '../db/segment';
 import * as resp from '../config/Responses';
-
+import { tokenizeString } from '../util/runTokenizer';
 
 export default (app) => {
   // get segment data
@@ -42,6 +42,15 @@ export default (app) => {
     segment.updateSegment(segmentIndex, documentId, machineTranslation, editModeTime, tileModeTime, voiceModeTime, totalEditTime, charactersEntered, wordsEntered, mode, (err, result) => {
       if (err) { next(err); return; }
       res.status(resp.good).send(result);
+    });
+  });
+
+  // turn string into a array of tokens
+  app.post('/api/tokenizer', passport.ensureAuthenticated, (req, res) => {
+    // body: { data: 'some string' }
+    const { data } = req.body;
+    tokenizeString(data, (tokens) => {
+      res.status(resp.good).json({ tokens });
     });
   });
 };
