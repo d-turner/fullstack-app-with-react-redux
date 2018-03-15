e67eb43ea05805c9188a597d2d7bb55c3709405a
Daniel Turner <dturner@tcd.ie>
2018-03-15 12:21:57 +0000

    huge refactor and reimplementation

--- client/src/components/DraggableTiles/Container/SortableTiles.js
+++ client/src/components/DraggableTiles/Container/SortableTiles.js
@@ -4,9 +4,57 @@ import PropTypes from 'prop-types';
 
 import SortableListItem from '../Presentation/SortableListItem';
 import styles from '../tile.scss';
+import Loader from '../../Loader/Loader';
+
+import { addEvent, removeEvent } from '../../../utils/eventsHelper';
+import { upperFirstLetter } from '../../../utils/stringParser';
 
 class SortableTiles extends React.Component {
-  state = { tempTile: null, overTrash: false }
+  state = { tempTile: null, overTrash: false, newPosition: undefined }
+
+  componentDidMount() {
+    const bin = document.querySelector('#trash');
+    addEvent(bin, 'dragenter', (e) => {
+      // need to set the style for the bin
+      console.log('add style');
+      e.preventDefault();
+      e.stopPropagation(); // stop it here to prevent it bubble up
+      this._onEnter(e);
+    });
+    addEvent(bin, 'dragover', (e) => {
+      console.log('bin dragover');
+      e.preventDefault(); // allows us to drop
+      e.stopPropagation(); // stop it here to prevent it bubble up
+    });
+    addEvent(bin, 'dragexit', (e) => {
+      console.log('bin dragexit');
+      e.stopPropagation(); // stop it here to prevent it bubble up
+      this._onLeave(e);
+    });
+    addEvent(bin, 'dragleave', (e) => {
+      console.log('remove style');
+      e.stopPropagation(); // stop it here to prevent it bubble up
+    });
+    addEvent(bin, 'drop', (e) => {
+      e.preventDefault();
+      e.stopPropagation(); // stop it here to prevent it bubble up
+      console.log('New position: ', this.state.newPosition);
+      if (this.state.newPosition) {
+        e.index = this.state.newPosition;
+      }
+      this._drop(e);
+      this.setState({ newPosition: undefined });
+    });
+  }
+
+  componentWillUnmount() {
+    const bin = document.querySelector('#trash');
+    removeEvent(bin, 'dragenter');
+    removeEvent(bin, 'dragover');
+    removeEvent(bin, 'dragexit');
+    removeEvent(bin, 'dragleave');
+    removeEvent(bin, 'drop');
+  }
 
   onEnd = (event) => {
     // need to move from oldIndex to new index
@@ -22,14 +70,12 @@ class SortableTiles extends React.Component {
     itemEl.parentNode.removeChild(itemEl);
   }
 
-  onMove = (event) => {
-    event.preventDefault();
-    this.setState({ dragWord: event.item.children[0].children[1].value });
+  onStart = () => {
     this.props.setDragging(true);
   }
 
-  removeWord = (index) => {
-    this.props.updateWord(index, '');
+  setPosition = (newPosition) => {
+    this.setState({ newPosition });
   }
 
   addTile = (index) => {
@@ -37,27 +83,28 @@ class SortableTiles extends React.Component {
   }
 
   _drop = (event) => {
-    event.preventDefault();
+    if (this.state.newPosition !== undefined) {
+      this.props.removeIndex( this.state.newPosition);
+      this.setState({ newPosition: undefined });
+      return;
+    }
     this.setState({ overTrash: false });
     let data = null;
     try {
       data = JSON.parse(event.dataTransfer.getData('text'));
-      this.removeWord(data.index);
+      this.props.removeIndex(data);
     } catch (e) {
       // If the text data isn't parsable we'll just ignore it.
-      return;
+      console.log('failed', e);
     }
   }
 
-  _preventDefault = (event) => {
-    event.preventDefault();
-    console.log(event);
-    this.setState({ overTrash: true });
+  _onEnter = (event) => {
+    if (!this.state.overTrash) this.setState({ overTrash: true });
   }
 
   _onLeave = (event) => {
-    event.preventDefault();
-    this.setState({ overTrash: false });
+    if (this.state.overTrash) this.setState({ overTrash: false });
   }
 
   sortableGroupDecorator = (componentBackingInstance) => {
@@ -65,19 +112,50 @@ class SortableTiles extends React.Component {
     if (componentBackingInstance) {
       const options = {
         draggable: 'li', // Specifies which items inside the element should be sortable
-        group: this.props.sortable ? // specifies how the sorting should work for that list
-          { name: 'shared', pull: 'clone', put: true }
-          : { name: 'shared', pull: 'clone', put: false },
-        sort: this.props.sortable,
+        group: { name: 'shared', pull: false, put: true },
+        sort: true,
+        touchStartThreshold: 20,
         animation: 300,
         ghostClass: styles.sortableGhost || 'sortable-ghost', // Class name for the drop placeholder
         chosenClass: styles.sortableChosen || 'sortable-chosen', // Class name for the chosen item
+        setData: (dataTransfer, dragEl) => {
+          dataTransfer.setData('text', dragEl.dataset.index);
+        },
         onEnd: this.onEnd,
         onAdd: this.onAdd,
-        onStart: this.onMove,
+        onStart: this.onStart,
+        onUpdate: (evt) => {
+          this.setState({ newPosition: evt.newIndex });
+        },
+        scrollSensitivity: 100,
       };
       Sortable.create(componentBackingInstance, options);
     }
+  }
+
+  trashDecorator = (componentBackingInstance) => {
+    // check if backing instance not null
+    if (componentBackingInstance) {
+      const options = {
+        draggable: 'li', // Specifies which items inside the element should be sortable
+        group: { name: 'shared', pull: false, put: false },
+        sort: false,
+        animation: 300,
+        ghostClass: styles.sortableGhost || 'sortable-ghost', // Class name for the drop placeholder
+        chosenClass: styles.sortableChosen || 'sortable-chosen', // Class name for the chosen item
+      };
+      Sortable.create(componentBackingInstance, options);
+    }
+  }
+
+  loading = () => {
+    return (
+      <div className="container">
+        <div className={styles.group}>
+          <Loader />
+        </div>
+      </div>
+    );
   }
 
   fakeEnter = (index, text) => {
@@ -87,7 +165,7 @@ class SortableTiles extends React.Component {
   fakeTile = (word, index) => {
     return (
       <SortableListItem
-        key={`${word}${index}${this.props.sortable}`}
+        key={`${word}${index}sortable`}
         value={word}
         index={index}
         itemIndex={index}
@@ -98,81 +176,83 @@ class SortableTiles extends React.Component {
       />
     );
   }
-
-  renderTile = (word, index) => {
+  firstFakeEnter = (_, text) => {
+    this.props.insertSourceWord(0, upperFirstLetter(text));
+    this.setState({ tempTile: null });
+  }
+  firstFakeTile = (word) => {
     return (
       <SortableListItem
-        key={`${word}${index}${this.props.sortable}`}
+        key={`${word}${0}sortable`}
+        setPosition={this.setPosition}
         value={word}
-        index={index}
-        itemIndex={index}
-        updateWord={this.props.updateWord}
-        editable={this.props.sortable}
-        addTile={this.addTile}
+        index={-1}
+        itemIndex={-1}
+        updateWord={this.firstFakeEnter}
+        addTile={() => {}}
+        insertTiles={this.props.insertTiles}
+        focus
       />
     );
   }
 
-  render() {
-    let id = 'non-sortable-list';
-    let backgroundClass = null;
-    if (this.props.sortable) id = 'sortable-list';
-    if (this.props.sortable && this.props.dragging) backgroundClass = styles.canDrag;
-    if (!this.props.sortable && this.props.dragging) backgroundClass = styles.dragging;
+  renderTile = (word, index) => {
     return (
-      <div className="container" ref={this.sortableContainersDecorator}>
+      <SortableListItem
+        key={`${word}${index}sortable`}
+        setPosition={this.setPosition}
+        value={word}
+        index={index}
+        itemIndex={index}
+        updateWord={this.props.updateWord}
+        addTile={this.addTile}
+        insertTiles={this.props.insertTiles} />
+    );
+  }
+
+  render() {
+    if (this.props.loading) return this.loading();
+    const id = 'sortable-list';
+    let backgroundClass = null;
+    if (this.props.dragging) backgroundClass = styles.canDrag;
+    return (
+      <div className="container">
         <div className={styles.group}>
-          <ol className={`${styles['group-list']} ${backgroundClass}`} id={id} ref={this.sortableGroupDecorator}>
+          <ol className={`${styles['group-list']} ${backgroundClass}`} id={id} ref={(ref) => { this.Tiles = ref; this.sortableGroupDecorator(ref);}}>
             {this.props.words.map((word, index) => {
               if (this.state.tempTile !== null && index === this.state.tempTile) {
                 return (
-                  <div key={`${word}${index}${this.props.sortable}123`} style={{ display: 'inline-block' }}>
+                  <div key={`${word}sortable123`} style={{ display: 'inline-block' }}>
                     {this.renderTile(word, index)}
                     {this.fakeTile('', index)}
+                  </div>
+                );
+              } else if (this.state.tempTile !== null && this.state.tempTile === -1 && index === 0) {
+                return (
+                  <div key={`${word}sortable123`} style={{ display: 'inline-block' }}>
+                    {this.firstFakeTile('')}
+                    {this.renderTile(word, index)}
                   </div>
                 );
               }
               return this.renderTile(word, index);
             })}
           </ol>
-          {this.props.sortable ? (
-            <span
-              className={this.state.overTrash ? styles.overTrash : styles.trash}
-              id="trash"
-              onDragOver={this._preventDefault}
-              onDragLeave={this._onLeave}
-              onDrop={this._drop}>
-              <i className={`material-icons ${styles.bin}`}>delete</i>
-              {this.state.overTrash ? fakeTilePresentation(this.state.dragWord) : null}
-            </span>
-          ) : null}
+          <span
+            className={this.state.overTrash ? styles.overTrash : styles.trash}
+            id="trash"
+            onDrop={this._drop}
+            ref={this.trashDecorator}>
+            <i className={`material-icons ${styles.bin}`}>delete</i>
+          </span>
         </div>
       </div>
     );
   }
 }
 
-function fakeTilePresentation(word) {
-  // if (word === undefined || word === null) return null;
-  // return (
-  //   <li className={`${styles.format} ${styles.noselect} ${styles.maxWidth}`}>
-  //     <div className={styles.tile}>
-  //       <span className={styles.grippy} />
-  //       <input className={styles.text}
-  //         style={{ width: `${word.length * 10 + 10}px`, minWidth: '40px' }}
-  //         type="text"
-  //         spellCheck="false"
-  //         cols="1"
-  //         rows="1"
-  //         defaultValue={word} />
-  //     </div>
-  //   </li>
-  // );
-  return null;
-}
 SortableTiles.propTypes = {
   words: PropTypes.arrayOf(PropTypes.string).isRequired,
-  sortable: PropTypes.bool.isRequired,
   updateWord: PropTypes.func.isRequired,
   onSortEnd: PropTypes.func.isRequired,
   insertSourceWord: PropTypes.func.isRequired,
--- client/src/components/DraggableTiles/Container/SortableTilesContainer.js
+++ client/src/components/DraggableTiles/Container/SortableTilesContainer.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { arrayMove } from 'react-sortable-hoc';
+import update from 'immutability-helper';
 
 import store from '../../../store';
 import * as actions from '../TileActions';
-import { splitTextIntoArray, cleanText, upperFirstLetter, lowerFirstLetter } from '../../../utils/stringParser';
+import { upperFirstLetter, lowerFirstLetter, insertAndReplace, insertIntoArray, shouldUpperCase, removeIndex } from '../../../utils/stringParser';
+import apiWrapper from '../../../utils/apiWrapper';
 import SortableTiles from './SortableTiles';
 
+// import apiWrapper from '../../../utils/apiWrapper';
+
 class SortableTilesContainer extends React.Component {
+  state = { words: [], loading: true, prev: [[]], next: [[]] }
+
   componentDidMount() {
     this.props.keyLogger.setTimer('tileStart');
+    apiWrapper.getTokens({ data: this.props.text }).then((res) => {
+      this.setState({
+        loading: false,
+        words: res.data.tokens,
+      });
+    });
   }
 
   componentWillUnmount() {
@@ -17,10 +29,9 @@ class SortableTilesContainer extends React.Component {
   }
 
   onSortEnd = ({ oldIndex, newIndex }) => {
-    console.log('old index', oldIndex);
-    console.log('new index', newIndex);
-    const targetWords = splitTextIntoArray(cleanText(this.props.segment.target));
-    const newArray = arrayMove(targetWords, oldIndex, newIndex);
+    if (newIndex === oldIndex) return;
+    const targetWords = this.state.words;
+    const newArray = arrayMove(targetWords, parseInt(oldIndex, 10), parseInt(newIndex, 10));
     // need to capitalize word at newIndex
     if (newIndex === 0) newArray[newIndex] = upperFirstLetter(newArray[newIndex]);
     // need to decapitalize word
@@ -28,62 +39,98 @@ class SortableTilesContainer extends React.Component {
       newArray[newIndex] = lowerFirstLetter(newArray[newIndex]);
       newArray[oldIndex] = upperFirstLetter(newArray[oldIndex]);
     }
+    this.updateWordArray(newArray);
+  }
+
+  undo = () => {
+    const { prev, words } = this.state;
+    if (prev.length === 0) return;
+    const newState = update(this.state, {
+      words: {
+        $set: prev[prev.length - 1],
+      },
+      prev: {
+        $splice: [[prev.length - 1, 1]],
+      },
+      next: {
+        $push: [words],
+      },
+    });
+    this.setState(newState);
+  }
+
+  redo = () => {
+    const { next, words } = this.state;
+    if (next.length === 0) return;
+    const newState = update(this.state, {
+      words: {
+        $set: next[next.length - 1],
+      },
+      prev: {
+        $push: [words],
+      },
+      next: {
+        $splice: [[next.length - 1, 1]],
+      },
+    });
+    this.setState(newState);
+  }
+
+  updateWordArray = (wordArray) => {
     store.dispatch(
       actions.updateWordOrder(
         this.props.documentId,
         this.props.segmentId,
-        newArray,
+        wordArray,
       ),
     );
+    const a1 = wordArray;
+    const a2 = this.state.words;
+    if (a1.length === a2.length && a1.every((v, i) => v === a2[i])) return;
+    this.setState({ words: wordArray, prev: [...this.state.prev, this.state.words], next: [] });
   }
 
   insertSourceWord = (index, text) => {
-    store.dispatch(
-      actions.insertSourceWord(
-        index,
-        text,
-        true,
-        this.props.segmentId,
-        this.props.documentId,
-      ),
-    );
+    if (shouldUpperCase(text, index)) text = upperFirstLetter(text);
+    const newArray = insertIntoArray(this.state.words, index, text.split(' '));
+    this.updateWordArray(newArray);
+  }
+
+  removeIndex = (index) => {
+    const newArray = removeIndex(this.state.words, index);
+    this.updateWordArray(newArray);
   }
 
   updateWord = (index, text) => {
-    store.dispatch(
-      actions.updateWord(
-        this.props.documentId,
-        this.props.segmentId,
-        index,
-        text,
-      ),
-    );
+    const newWords = insertAndReplace(this.state.words, index, text.split(' '));
     const event = { index, text };
     this.props.keyLogger.updateWord(event);
+    this.updateWordArray(newWords);
   }
 
   render() {
     return (
       <SortableTiles
-        words={this.props.words}
-        sortable={this.props.sortable}
+        words={this.state.words}
         insertSourceWord={this.insertSourceWord}
         updateWord={this.updateWord}
+        removeIndex={this.removeIndex}
         onSortEnd={this.onSortEnd}
         setDragging={this.props.setDragging}
-        dragging={this.props.dragging} />
+        dragging={this.props.dragging}
+        loading={this.state.loading}
+        insertTiles={this.props.insertTiles} />
     );
   }
 }
 
 SortableTilesContainer.propTypes = {
-  words: PropTypes.arrayOf(PropTypes.string).isRequired,
-  sortable: PropTypes.bool.isRequired,
-  segment: PropTypes.objectOf(PropTypes.any).isRequired,
+  text: PropTypes.string.isRequired,
   segmentId: PropTypes.number.isRequired,
   documentId: PropTypes.string.isRequired,
   keyLogger: PropTypes.objectOf(PropTypes.any).isRequired,
   dragging: PropTypes.bool.isRequired,
+  insertTiles: PropTypes.bool.isRequired,
   setDragging: PropTypes.func.isRequired,
 };
 
