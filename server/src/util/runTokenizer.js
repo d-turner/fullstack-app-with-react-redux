f93abcca7b7a98a453fd6f5d49142147ad1bab42
Daniel Turner <dturner@tcd.ie>
2018-04-03 11:07:21 +0100

    add fix for accent characters

--- server/src/util/runTokenizer.js
+++ server/src/util/runTokenizer.js
@@ -1,10 +1,13 @@
 const spawn = require('child_process').spawn;
 
 export const tokenizeString = (sentence, callback) => {
-  const pythonProcess = spawn('./pythonDependencies/bin/python', ['./src/util/tokenizer.py', sentence]);
+  const pythonProcess = spawn('/home/adapt/Documents/git/kanjingo-react-redux/server/pythonDependencies/bin/python', ['/home/adapt/Documents/git/kanjingo-react-redux/server/src/util/tokenizer.py', sentence]);
 
   pythonProcess.stdout.on('data', (data) => {
-    if (data.toString() === 'ZZZXXXZZZXXX') return callback([]);
-    return callback(data.toString().split('ZZZXXXZZZXXX'));
+    let dataString = data.toString();
+    dataString = dataString.replace(/``/g, '"');
+    dataString = dataString.replace(/''/g, '"');
+    if (dataString === 'ZZZXXXZZZXXX') return callback([]);
+    return callback(dataString.split('ZZZXXXZZZXXX'));
   });
 };
