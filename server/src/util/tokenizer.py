9f0dfebc7a2a362303b1627256ee4331106c29f9
Daniel Turner <dturner@tcd.ie>
2018-04-26 15:38:17 +0100

    use tweet tokenizer

--- server/src/util/tokenizer.py
+++ server/src/util/tokenizer.py
@@ -1,10 +1,13 @@
 import sys
 import nltk
 
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize, WhitespaceTokenizer, RegexpTokenizer, TweetTokenizer
 # nltk.download('punkt')
 s1 = sys.argv[1]
-words = word_tokenize(s1)
+# words = word_tokenize(s1)
+tknzr = TweetTokenizer()
+#tknzr = WhitespaceTokenizer()
+words = tknzr.tokenize(s1)
 if (words == []):
   sys.stdout.write("ZZZXXXZZZXXX")
 else:
